7e5850533abfc5943a2349f3cdfd1a0cc0f98b1c
Benedikt Schmidt <beschmi@gmail.com>
2012-01-24 19:58:08 +0000

    add missing file
    git-svn-id: https://svn.inf.ethz.ch/svn/basin/infsec/trunk/projects/FOSSP/tools/tamarin-prover@83549 7e91533c-85bf-0310-9ad9-9686b5989d42

---
+++ lib/utils/src/Debug/Trace/Ignore.hs
@@ -0,0 +1,16 @@
+-- |
+-- Copyright   : (c) 2011 Benedikt Schmidt
+-- License     : GPL v3 (see LICENSE)
+-- 
+-- Maintainer  : Benedikt Schmidt <beschmi@gmail.com>
+-- Portability : GHC only
+--
+-- Module that can be imported to ignore calls to @trace@.
+module Debug.Trace.Ignore (
+    trace
+  ) where
+
+-- | @trace a b@ returns the second argument.
+trace :: a -> b -> b
+trace = flip const
+
