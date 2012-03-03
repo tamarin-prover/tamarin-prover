64dae57aab75994502564455b392ecab2ac5deee
Simon Meier <iridcode@gmail.com>
2012-03-03 13:31:08 +0100

    implemented computation of minimal loop-breakers of a finite relation

--- lib/utils/src/Data/DAG/Simple.hs
+++ lib/utils/src/Data/DAG/Simple.hs
@@ -1,23 +1,36 @@
 -- |
--- Copyright   : (c) 2010 Simon Meier
+-- Copyright   : (c) 2010,2012 Simon Meier
 -- License     : GPL v3 (see LICENSE)
 -- 
 -- Maintainer  : Simon Meier <iridcode@gmail.com>
 --
 -- Simple  vertice list based representation of DAGs and some common operations on it.
 module Data.DAG.Simple (
-    toposort
+  -- * Computing with binary relations
+    Relation
+  , inverse
+  , image
   , reachableSet
+  , restrict
+
+  -- ** Cycles
+  , dfsLoopBreakers
   , cyclic
+  , toposort
+
 ) where
 
 
-import Data.List
-import qualified Data.Set as S
+import           Control.Basics
+import           Control.Monad.Writer
+import           Control.Monad.RWS
 
-import Control.Basics
-import Control.Monad.Writer
+import           Data.List
+import qualified Data.DList as D
+import qualified Data.Set   as S
 
+-- import Test.QuickCheck.Property (label)
+-- import Test.QuickCheck (quickCheck)
 
 -- | Produce a topological sorting of the given relation. If the relation is
 -- cyclic, then the result is at least some permutation of all elements of
@@ -31,7 +44,7 @@ toposort dag =
       | otherwise            =
           foldM visit (S.insert x visited) preds <* tell (pure x)
       where
-        preds = [ e | (e,e') <- dag, e' == x ]
+        preds = x `image` inverse dag
 
 
 -- | Compute the set of nodes reachable from the given set of nodes.
@@ -42,9 +55,7 @@ reachableSet start dag =
     visit visited x 
       | x `S.member` visited = visited
       | otherwise            =
-          foldl' visit (S.insert x visited) succs
-      where
-        succs = [ e' | (e,e') <- dag, e == x ]
+          foldl' visit (S.insert x visited) (x `image` dag)
 
 -- | Is the relation cyclic.
 cyclic :: Ord a => [(a,a)] -> Bool
@@ -64,3 +75,59 @@ cyclic rel =
         next     = [ e' | (e,e') <- rel, e == x ]
         parents' = S.insert x parents
 
+
+-- | Compute a minimal set of loop-breakers using a greedy DFS strategy. A set
+-- of loop-breakers is a set of nodes such that removing them ensures the
+-- acyclicity of the relation. It is minimal, if no node can be removed from
+-- the set.
+dfsLoopBreakers :: Ord a => [(a,a)] -> [a]
+dfsLoopBreakers rel = 
+    D.toList $ snd $ execRWS (mapM_ (visit . fst) rel) () S.empty
+  where 
+    visit x = do
+        visited <- gets (S.member x)
+        unless visited $ findLoopBreakers S.empty x
+
+    -- PRE: x0 is not yet visited
+    findLoopBreakers parents0 x = do
+        modify (S.insert x)
+        let parents = S.insert x parents0
+            ys      = x `image` rel
+        if any (`S.member` parents) ys
+          then tell (return x) 
+          else forM_ ys $ \y -> do 
+                   visited <- gets (S.member y)
+                   unless visited $ findLoopBreakers parents y
+
+-- | A relation represented as a list of tuples.
+type Relation a = [(a,a)]
+
+-- | Restrict a relation to elements satisfying a predicate.
+restrict :: Eq a => (a -> Bool) -> Relation a -> Relation a
+restrict p = filter (\(x,y) -> p x && p y)
+
+-- | The image of an element under a 'Relation'.
+image :: Eq a => a -> Relation a -> [a]
+image x rel = [ y' | (x', y') <- rel, x == x' ]
+
+-- | The inverse of a 'Relation'.
+inverse :: Relation a -> Relation a
+inverse rel = [ (y,x) | (x, y) <- rel ]
+
+{-
+prop_dfsLoopBreakers noSelfLoops rel0
+  | cyclic rel = label cycleLabel (minimal && not (cyclic (rel' breakers)))
+  | otherwise  = label "acyclic" (null breakers)
+  where
+    -- remove (x,x) entries in half of the cases
+    removeSelfLoops | noSelfLoops = filter (not . (uncurry (==)))
+                    | otherwise   = id
+    -- restrict the relation to get more cyclic cases
+    rel      =  removeSelfLoops $ map ((`mod` (20 :: Int)) *** (`mod` 20)) rel0
+    rel' bs  = restrict (not . (`elem` bs)) rel
+    breakers = dfsLoopBreakers rel
+    cycleLabel | any (uncurry (==)) rel = "cyclic (with self-loop)"
+               | otherwise              = "cyclic (no self-loop)"
+    -- true if loop breakers are minimal
+    minimal = all (\b -> cyclic (rel' (filter (/= b) breakers))) breakers
+-}
