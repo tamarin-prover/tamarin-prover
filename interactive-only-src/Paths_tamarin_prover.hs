78482f94ff3f74c4eaf7ef782709a986ea279ef9
Simon Meier <iridcode@gmail.com>
2012-04-11 23:29:21 +0200

    wibble in development source files

--- interactive-only-src/Paths_tamarin_prover.hs
+++ interactive-only-src/Paths_tamarin_prover.hs
@@ -12,7 +12,7 @@ catchIO = Exception.catch
 
 
 version :: Version
-version = Version {versionBranch = [0,1,0], versionTags = []}
+version = Version {versionBranch = [0,3,0,0], versionTags = []}
 bindir, libdir, datadir, libexecdir :: FilePath
 
 bindir     = "./"
