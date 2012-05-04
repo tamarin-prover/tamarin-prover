0788a54704e79c81027e29709f834de85cb9115c
Simon Meier <iridcode@gmail.com>
2012-05-04 12:24:06 +0200

    Revert "decrease version numbers"; back at 0.5.0.0
    This reverts commit 0e39c2b70114913d9930e964b321e94aab81a93d.

--- interactive-only-src/Paths_tamarin_prover.hs
+++ interactive-only-src/Paths_tamarin_prover.hs
@@ -12,7 +12,7 @@ catchIO = Exception.catch
 
 
 version :: Version
-version = Version {versionBranch = [0,4,1,0], versionTags = []}
+version = Version {versionBranch = [0,5,0,0], versionTags = []}
 bindir, libdir, datadir, libexecdir :: FilePath
 
 bindir     = "./"
