a4086d38a00830faeb3fa8431c29ea802ff477d2
Jannik Dreier <jannik.dreier@loria.fr>
2016-11-28 15:29:01 +0100

    new liveness ranking

--- lib/theory/src/Theory/Constraint/Solver/ProofMethod.hs
+++ lib/theory/src/Theory/Constraint/Solver/ProofMethod.hs
@@ -359,6 +359,7 @@ data GoalRanking =
     GoalNrRanking
   | OracleRanking
   | SapicRanking
+  | SapicLivenessRanking
   | SapicPKCS11Ranking
   | UsefulGoalNrRanking
   | SmartRanking Bool
@@ -373,11 +374,12 @@ goalRankingName ranking =
         GoalNrRanking                -> "their order of creation"
         OracleRanking                -> "an oracle for ranking"
         UsefulGoalNrRanking          -> "their usefulness and order of creation"
-        SapicRanking                -> "heuristics adapted to the output of the SAPIC tool"
-        SapicPKCS11Ranking          -> "heuristics adapted to a model of PKCS#11 translated using the SAPIC tool"
+        SapicRanking                 -> "heuristics adapted to the output of the SAPIC tool"
+        SapicLivenessRanking         -> "heuristics adapted to the output of the SAPIC tool for liveness properties"
+        SapicPKCS11Ranking           -> "heuristics adapted to a model of PKCS#11 translated using the SAPIC tool"
         SmartRanking useLoopBreakers -> smart useLoopBreakers
         SmartDiffRanking             -> "the 'smart' heuristic (for diff proofs)"
-        InjRanking                  -> "heuristics adapted to stateful injective protocols"
+        InjRanking                   -> "heuristics adapted to stateful injective protocols"
    where
      smart b = "the 'smart' heuristic (loop breakers " ++
                (if b then "allowed" else "delayed") ++ ")."
@@ -528,6 +530,160 @@ sapicRanking :: ProofContext
               -> System
               -> [AnnotatedGoal] -> [AnnotatedGoal]
 sapicRanking ctxt sys =
+    sortOnUsefulness . unmark . sortDecisionTree solveLast . sortDecisionTree solveFirst . goalNrRanking
+  where
+    oneCaseOnly = catMaybes . map getMsgOneCase . L.get pcCaseDists $ ctxt
+
+    getMsgOneCase cd = case msgPremise (L.get cdGoal cd) of
+      Just (viewTerm -> FApp o _)
+        | length (getDisj (L.get cdCases cd)) == 1 -> Just o
+      _                                            -> Nothing
+
+    sortOnUsefulness = sortOn (tagUsefulness . snd . snd)
+
+    unmark = map unmarkPremiseG
+    unmarkPremiseG (goal@(PremiseG _ _), (nr, _)) = (goal, (nr, Useful))
+    unmarkPremiseG annGoal                        = annGoal
+
+    tagUsefulness Useful                = 0 :: Int
+    tagUsefulness ProbablyConstructible = 1
+    tagUsefulness LoopBreaker           = 0
+    tagUsefulness CurrentlyDeducible    = 2
+
+    solveLast = 
+        [ 
+        -- isNotInsertAction . fst 
+        -- ,
+        isNonLastProtoFact . fst ,
+        isNotKnowsLastNameGoal . fst,
+        isNotLastInsertAction . fst
+        ]
+        -- move the Last proto facts (L_) to the end.
+
+    solveFirst =
+        [ isChainGoal . fst
+        , isDisjGoal . fst
+        , isFirstProtoFact . fst
+        , isStateFact . fst
+        , isUnlockAction . fst
+        , isKnowsFirstNameGoal . fst
+        , isFirstInsertAction . fst
+        , isNonLoopBreakerProtoFactGoal
+        , isStandardActionGoalButNotInsert  . fst
+        , isNotAuthOut . fst
+        , isPrivateKnowsGoal . fst
+        -- , isFreshKnowsGoal . fst
+        , isSplitGoalSmall . fst
+        , isMsgOneCaseGoal . fst
+        , isDoubleExpGoal . fst
+        , isNoLargeSplitGoal . fst 
+        ]
+        -- move the rest (mostly more expensive KU-goals) before expensive
+        -- equation splits
+
+    -- FIXME: This small split goal preferral is quite hacky when using
+    -- induction. The problem is that we may end up solving message premise
+    -- goals all the time instead of performing a necessary split. We should make
+    -- sure that a split does not get too old.
+    smallSplitGoalSize = 3
+
+    isNonLoopBreakerProtoFactGoal (PremiseG _ fa, (_, Useful)) =
+       not (isKFact fa) && not (isAuthOutFact fa)
+    isNonLoopBreakerProtoFactGoal _                            = False
+
+    isAuthOutFact (Fact (ProtoFact _ "AuthOut" _) _) = True
+    isAuthOutFact  _                                 = False
+
+    isStateFact (PremiseG _ (Fact (ProtoFact _ n _) _)) = isPrefixOf "State_" n
+    isStateFact  _                                 = False
+
+    isUnlockAction (ActionG _ (Fact (ProtoFact _ "Unlock" _) _)) = True
+    isUnlockAction  _                                 = False
+
+    isFirstInsertAction (ActionG _ (Fact (ProtoFact _ "Insert" _)  (t:_)) ) = 
+        case t of
+            (viewTerm2 -> FPair (viewTerm2 -> Lit2( Con (Name PubName a)))  _) -> isPrefixOf "F_" (show a)
+            _ -> False
+    isFirstInsertAction _ = False
+
+    isNotLastInsertAction (ActionG _ (Fact (ProtoFact _ "Insert" _)  (t:_)) ) = 
+        case t of
+            (viewTerm2 -> FPair (viewTerm2 -> Lit2( Con (Name PubName a)))  _) -> not( isPrefixOf "L_" (show a))
+            _ -> True
+    isNotLastInsertAction _ = True
+
+    isNotInsertAction (ActionG _ (Fact (ProtoFact _ "Insert" _) _)) = False
+    isNotInsertAction  _                                 = True
+
+    isStandardActionGoalButNotInsert g = 
+       (isStandardActionGoal g) &&  (isNotInsertAction g)
+
+
+    isNonLastProtoFact (PremiseG _ (Fact (ProtoFact _ ('L':'_':_) _) _)) = False
+    isNonLastProtoFact _                                                 = True
+
+    isFirstProtoFact (PremiseG _ (Fact (ProtoFact _ ('F':'_':_) _) _)) = True
+    isFirstProtoFact _                                                 = False
+
+    isNotAuthOut (PremiseG _ fa) = not (isAuthOutFact fa)
+    isNotAuthOut _               = False
+
+    msgPremise (ActionG _ fa) = do (UpK, m) <- kFactView fa; return m
+    msgPremise _              = Nothing
+
+--  Problematic when using handles.
+--    isFreshKnowsGoal goal = case msgPremise goal of
+--        Just (viewTerm -> Lit (Var lv)) | lvarSort lv == LSortFresh -> True
+--        _                                                           -> False
+    -- we recognize any variable starting with h as a handle an deprioritize 
+    isLastName lv = isPrefixOf "L_" (lvarName lv)
+
+    isFirstName lv = isPrefixOf "F_" (lvarName lv)
+
+    isNotKnowsLastNameGoal goal = case msgPremise goal of
+        Just (viewTerm -> Lit (Var lv)) | ((lvarSort lv  == LSortFresh) && isLastName lv)-> False
+        _                                                           -> True
+
+    isKnowsFirstNameGoal goal = case msgPremise goal of
+        Just (viewTerm -> Lit (Var lv)) | ((lvarSort lv  == LSortFresh) && isFirstName lv)-> True
+        _                                                           -> False
+
+
+    isMsgOneCaseGoal goal = case msgPremise goal of
+        Just (viewTerm -> FApp o _) | o `elem` oneCaseOnly -> True
+        _                                                  -> False
+
+    isPrivateKnowsGoal goal = case msgPremise goal of
+        Just t -> isPrivateFunction t
+        _      -> False
+
+    isDoubleExpGoal goal = case msgPremise goal of
+        Just (viewTerm2 -> FExp  _ (viewTerm2 -> FMult _)) -> True
+        _                                                  -> False
+
+    -- Be conservative on splits that don't exist.
+    isSplitGoalSmall (SplitG sid) =
+        maybe False (<= smallSplitGoalSize) $ splitSize (L.get sEqStore sys) sid
+    isSplitGoalSmall _            = False
+
+    isNoLargeSplitGoal goal@(SplitG _) = isSplitGoalSmall goal
+    isNoLargeSplitGoal _               = True
+
+    -- | @sortDecisionTree xs ps@ returns a reordering of @xs@
+    -- such that the sublist satisfying @ps!!0@ occurs first,
+    -- then the sublist satisfying @ps!!1@, and so on.
+    sortDecisionTree :: [a -> Bool] -> [a] -> [a]
+    sortDecisionTree []     xs = xs
+    sortDecisionTree (p:ps) xs = sat ++ sortDecisionTree ps nonsat
+      where (sat, nonsat) = partition p xs
+
+    
+-- | A ranking function tuned for the automatic verification of
+-- protocols with liveness properties generated with the Sapic tool
+sapicLivenessRanking :: ProofContext
+                     -> System
+                     -> [AnnotatedGoal] -> [AnnotatedGoal]
+sapicLivenessRanking ctxt sys =
     sortOnUsefulness . unmark . sortDecisionTree solveLast . sortDecisionTree solveFirst . goalNrRanking
   where
     oneCaseOnly = catMaybes . map getMsgOneCase . L.get pcCaseDists $ ctxt
