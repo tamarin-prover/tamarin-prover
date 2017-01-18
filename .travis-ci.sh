afb02accf0042156fdc713210dda3b1e3a86a8d2
Jannik Dreier <jannik.dreier@loria.fr>
2017-01-18 15:50:46 +0100

    travis CI integration

---
+++ .travis-ci.sh
@@ -0,0 +1,22 @@
+# Edit this for your own project dependencies
+OPAM_DEPENDS="ocamlfind ounit re"
+	 
+case "$OCAML_VERSION,$OPAM_VERSION" in
+3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
+3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+	 
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+opam init 
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+make
+make test
