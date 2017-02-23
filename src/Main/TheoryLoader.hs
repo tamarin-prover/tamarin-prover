74797d8a28d86d6ea54f3392544ce25f0836981c
Ralf Sasse <ralf.sasse@gmail.com>
2017-02-23 14:29:34 +0100

    The oracle ranking is now passed through in diff mode and can be used for the Observational_equivalence lemma.

--- src/Main/TheoryLoader.hs
+++ src/Main/TheoryLoader.hs
@@ -346,12 +346,14 @@ constructAutoDiffProver as =
 
     ranking 's' = SmartRanking False
     ranking 'S' = SmartRanking True
+    ranking 'o' = OracleRanking
     ranking 'c' = UsefulGoalNrRanking
     ranking 'C' = GoalNrRanking
     ranking r   = error $ render $ fsep $ map text $ words $
       "Unknown goal ranking '" ++ [r] ++ "'. Use one of the following:\
       \ 's' for the smart ranking without loop breakers,\
       \ 'S' for the smart ranking with loop breakers,\
+      \ 'o' for oracle ranking,\
       \ 'c' for the creation order and useful goals first,\
       \ and 'C' for the creation order."
 
