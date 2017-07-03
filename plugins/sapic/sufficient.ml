f04f4d589ac820863b03edc6e64a6e93ad44ddd6
Robert Künnemann <robert.kuennemann@cased.de>
2017-07-03 14:30:05 +0200

    Added support for minimality lemmas.

--- plugins/sapic/sufficient.ml
+++ plugins/sapic/sufficient.ml
@@ -30,6 +30,19 @@ let exhaustiveness id vf =
     in
         ForallLemma  (label,disj)
 
+let dishonest parties b = 
+    let corrupted_a p i = Atom ( At ( (Action ("Corrupted",[Var p])), (Temp ("c"^string_of_int(i))))) in
+    match 
+            VarSet.fold (fun p (a,i) -> 
+        And ((if (VarSet.mem p b) then (corrupted_a p i) else Not (corrupted_a p i)),a),i+1)
+            parties
+            (Atom True, 0) 
+    with (r,_)->r
+
+let dishonest_disj parties v = 
+    let disj = List.fold_left (fun a b -> Or(dishonest parties b,a)) (Atom False) v in
+    Ex (free_vars VarSet.empty disj,disj)
+
 let sufficiency id parties vf phi = 
 (* for the each mapping φ_i → V_i *) 
 (* where V_i = B_i^1 | .. | B_i^n *)
@@ -37,22 +50,37 @@ let sufficiency id parties vf phi =
 (* ( φ_i && ( dishonest(B_i^1) | .. | dishonest(B_i^n)) && not (φ) ) *)
     let sufficient i (f,v) = 
         let label = Printf.sprintf "lemma %s_suf_%n" id i in
-        let corrupted_a p i = Atom ( At ( (Action ("Corrupted",[Var p])), (Temp ("c"^string_of_int(i))))) in
-        let dishonest b = 
-            match 
-            VarSet.fold (fun p (a,i) -> 
-            And ((if (VarSet.mem p b) then (corrupted_a p i) else Not (corrupted_a p i)),a),i+1)
-            parties
-            (Atom True, 0) 
-            with (r,_)->r
-        in
-        let dishonest_disj v = 
-            let disj = List.fold_left (fun a b -> Or(dishonest b,a)) (Atom False) v in
-            Ex (free_vars VarSet.empty disj,disj)
-        in
-        ExistsLemma (label, And(f,And(dishonest_disj v,Not(phi))))
+        ExistsLemma (label, And(f,And(dishonest_disj parties v,Not(phi))))
     in
         mapi sufficient vf 
+
+let minimality id parties vf phi = 
+(* for the each mapping φ_i → V_i *) 
+(* where V_i = B_i^1 | .. | B_i^n *)
+(* and for all strict subsets B' of some B_i^j: *)
+(* (min-i) Minimality of V_i: forall-trace *)
+(* not ( φ_i && Dishonest(B') ) *)
+(* TODO not sure, maybe instead need *)
+(* not ( φ_i && φ && Dishonest(B') ) *)
+    (* let rec list_of_subsets b = *) 
+    (*     if VarSet.is_empty b then [b] *)
+    (*     else List.fold_left (fun a elem -> (list_of_subsets (VarSet.remove elem b))@a ) [b] (VarSet.elements b) *)
+    (* in *)
+    let list_of_immeadeate_subsets b =
+        List.map (fun e -> VarSet.remove e b) (VarSet.elements b)
+    in
+    let minimal f i j k b' = 
+        let label = Printf.sprintf "lemma %s_min_%n_%n_%n" id i j k in
+        ForallLemma (label, Not(And(f,dishonest parties b')))
+    in
+        List.flatten
+        ( List.flatten
+        (mapi 
+        (fun i (f,v) -> mapi 
+            (fun j b -> 
+                mapi (minimal f i j) (list_of_immeadeate_subsets b)) 
+            v)
+        vf))
 
 let sufficient_conditions id parties vf phi =
     (exclusiveness id vf )
@@ -60,4 +88,6 @@ let sufficient_conditions id parties vf phi =
     [exhaustiveness id vf]
     @
     (sufficiency id parties vf phi)
+    @
+    (minimality id parties vf phi)
 
