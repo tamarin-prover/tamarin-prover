ff4362bab2204e8d417dd7246645bf5430a832f9
alexander.dax <alexander.dax@cispa.saarland>
2019-02-18 16:19:39 +0100

    wip: minir changes in Sapic.hs

--- lib/sapic/src/Sapic.hs
+++ lib/sapic/src/Sapic.hs
@@ -13,22 +13,13 @@
 module Sapic (
     translate
 ) where
-<<<<<<< HEAD
-import Text.ParserCombinators.Parsec
-import Theory.Text.Parser
 import Theory
-translate x = x
-
-
--- translate :: Either ParseError OpenTheory -> Either ParseError OpenTheory
--- translate x = parseOpenTheoryString ["lol","fail"] "fail"
-=======
 
 translate (Left x) = x
-translate (Right th) = 
-    fold_left (addProtoRule th) th []
+translate (Right th) =
+    foldr addProtoRule th msrs
     where
-        process = theoryProcesses th
-        msrs =  []
--- translate x = Left (Parsec.ParseError "Lol")
->>>>>>> d51d092d5f8d1ba2f34bd6156a2f23b5a66de67e
+        processes = theoryProcesses th
+        msrs =  map msrFromProcess processes
+
+msrFromProcess process = []
