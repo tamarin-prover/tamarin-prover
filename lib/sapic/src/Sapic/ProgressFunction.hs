0309f190f6b4bb58b6cd87feffc4cc5acd9429dd
Robert Künnemann <robert.kuennemann@cased.de>
2019-03-22 17:30:25 +0100

    working in progress function.

--- lib/sapic/src/Sapic/ProgressFunction.hs
+++ lib/sapic/src/Sapic/ProgressFunction.hs
@@ -30,11 +30,14 @@ import qualified Data.Map.Strict as M
 type ProgressFunction = M.Map ProcessPosition (S.Set (S.Set ProcessPosition))
  
  -- | suffix list p to each element of set *)
+(<.>) :: Ord a => S.Set [a] -> [a] -> S.Set [a]
 (<.>) set pos = S.map (\pos' -> pos' ++ pos) set 
 
 -- | suffix list p to each element in a set of set of sets *)
+(<..>) :: Ord a => S.Set (S.Set [a]) -> [a] -> S.Set (S.Set [a])
 (<..>) setset pos = S.map (\set' -> set' <.> pos) setset
 
+blocking :: (Num a, Ord a) => AnProcess ann -> Maybe (S.Set [a])
 blocking (ProcessNull _) = Just (S.singleton [1])
 blocking (ProcessAction Rep _ _ ) = Just (S.singleton [1])
 blocking (ProcessAction (ChIn _ _) _ _) = Just (S.singleton [1])
@@ -43,33 +46,35 @@ blocking (ProcessComb NDC _ pl pr)
     | otherwise = Nothing
 blocking _ =  Nothing
 
--- progressTo  p -- f within generate progressfunction.ml
---     | (ProcessNull _) <- p = ss [1]
---     | (ProcessAction Rep _ _ ) <-p = ss [1]
---     | (ProcessAction (ChIn _ _) _ _) <-p = ss [1]
---     | (ProcessComb (Cond _) _ pl pr) <- p = combine (f pl  <.> [1]) (f pr <.> [2])  
---     | (ProcessComb (CondEq _ _) _ pl pr) <- p = combine (f pl  <.> [1]) (f pr <.> [2])  
---     | (ProcessComb (Lookup _ _) _ pl pr) <- p = combine (f pl  <.> [1]) (f pr <.> [2])  
---     | (ProcessComb NDC _ pl pr) <- p
---     , Just psl <- blocking pl, Just psr <- blocking pr = ss [] 
---     | (ProcessComb NDC _ pl pr) <- p
---     , Just psl <- blocking pl, Nothing <- blocking pr =  
---               S.foldr 
--- 	      (\ pos pss -> combine (f (processAt p pos) <..> pos) pss )
---               (psl <.> [1])
--- 	      (f pr <..> [2]) 
---     | (ProcessComb NDC _ pl pr) <- p
---     , Nothing <- blocking pl, Just psr <- blocking pr = 
--- 	      S.foldr
---               (\ pos pss -> combine (f (processAt p pos) <..> pos) pss )
--- 	      (psr <.> [2])
--- 	      (f pl <..> [1])
---     | (ProcessComb NDC _ pl pr) <- p
---     , Nothing <- blocking pl, Nothing <- blocking pr = 
---             combine (f pl <..> [1] ) (f r @@. [2])
---     | (ProcessComb _  _ pl pr) <- p =  S.union (f pl <..> [1]) (f pr <..> [2]) 
---     | (ProcessAction _ _ p')  <- p = f p' <..> [1]
---     where ss = S.singleton . S.singleton
---           f  =  progressTo  -- shortcut for recursive call
---           combine_with y x_i set1 = S.fold (\y_i set2 -> (x_i `S.union` y_i) `S.insert` set2)
---           combine = S.fold (combine_with y) x S.empty
+combine_with y x_i set1 = S.fold (\y_i set2 -> (x_i `S.union` y_i) `S.insert` set2)
+
+combine x y = S.fold (combine_with y) x S.empty
+
+progressTo  p -- f within generate progressfunction.ml
+    | (ProcessNull _) <- p = ss [1]
+    | (ProcessAction Rep _ _ ) <-p = ss [1]
+    | (ProcessAction (ChIn _ _) _ _) <-p = ss [1]
+    -- | (ProcessComb (Cond _) _ pl pr) <- p = combine (f pl  <.> [1]) (f pr <.> [2])  
+    -- | (ProcessComb (CondEq _ _) _ pl pr) <- p = combine (f pl  <.> [1]) (f pr <.> [2])  
+    -- | (ProcessComb (Lookup _ _) _ pl pr) <- p = combine (f pl  <.> [1]) (f pr <.> [2])  
+    -- | (ProcessComb NDC _ pl pr) <- p
+    -- , Just psl <- blocking pl, Just psr <- blocking pr = ss [] 
+    -- | (ProcessComb NDC _ pl pr) <- p
+    -- , Just psl <- blocking pl, Nothing <- blocking pr =  
+    --           S.foldr 
+	      -- (\ pos pss -> combine (f (processAt p pos) <..> pos) pss )
+    --           (psl <.> [1])
+	      -- (f pr <..> [2]) 
+    -- | (ProcessComb NDC _ pl pr) <- p
+    -- , Nothing <- blocking pl, Just psr <- blocking pr = 
+	      -- S.foldr
+    --           (\ pos pss -> combine (f (processAt p pos) <..> pos) pss )
+	      -- (psr <.> [2])
+	      -- (f pl <..> [1])
+    -- | (ProcessComb NDC _ pl pr) <- p
+    -- , Nothing <- blocking pl, Nothing <- blocking pr = 
+    --         combine (f pl <..> [1] ) (f pr <..> [2])
+    -- | (ProcessComb _  _ pl pr) <- p =  S.union (f pl <..> [1]) (f pr <..> [2]) 
+    | (ProcessAction _ _ p')  <- p = f p' <..> [1]
+    where ss x = S.singleton ( S.singleton x)
+          f  =  progressTo  -- shortcut for recursive call
