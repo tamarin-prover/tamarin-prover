94c2cc06510947cff0d01ff4f435857e5cd40e63
Charlie Jacomme <jacomme@crans.org>
2021-02-24 16:21:08 +0100

    Fix export of states

--- lib/export/src/States.hs
+++ lib/export/src/States.hs
@@ -18,6 +18,7 @@ import         Theory
 import         Theory.Sapic
 import qualified  Data.Set as S
 import qualified Data.Map as M
+import qualified Data.List as L
 import           Control.Monad.Fresh
 
 type StateMap = M.Map SapicTerm SapicLVar
@@ -27,7 +28,7 @@ addStatesChannels :: PlainProcess -> (PlainProcess, StateMap)
 addStatesChannels p = (p', stateMap)
  where
    allStates = getAllStates p
-   (p', stateMap) = evalFresh (declareStateChannel p (S.toList allStates) S.empty M.empty) 0
+   (p', stateMap) =  evalFresh (declareStateChannel p (S.toList allStates) S.empty M.empty) 0
 
 
 getAllStates :: PlainProcess -> (S.Set SapicTerm)
@@ -41,28 +42,37 @@ getAllStates (ProcessComb _ _ pl pr) = (getAllStates pl) `S.union` (getAllStates
 -- Descends into a process. Whenever all the names of a state term are declared, we declare a name corresponding to this state term, that will be used as the corresponding channel name.
 declareStateChannel ::  MonadFresh m => PlainProcess -> [SapicTerm] -> (S.Set SapicLVar) -> StateMap -> m (PlainProcess,  M.Map SapicTerm SapicLVar)
 declareStateChannel p [] _ stateMap = return (p, stateMap)
-declareStateChannel p tD@(v:toDeclare) boundNames stateMap =
-  if (S.fromList $ freesSapicTerm v) `S.isSubsetOf` boundNames then  do
-    newvar <- freshLVar "StateChannel" LSortMsg
-    let  newslvar = SapicLVar newvar (Just "channel")
-    let newMap =  M.insert v newslvar stateMap
-    (p', newMap')<- declareStateChannel p toDeclare boundNames newMap
-    return (ProcessAction (New newslvar) mempty p', newMap')
-  else
+declareStateChannel p toDeclare boundNames stateMap =
+  let (declarables, undeclarables) =  L.partition (\v -> (S.fromList $ freesSapicTerm v) `S.isSubsetOf` boundNames) toDeclare in
+  if declarables == [] then  do
     case p of
       ProcessNull _ -> return (p, stateMap)
       ProcessComb a b pl pr -> do
-        (pl', lMap) <- declareStateChannel pl tD boundNames stateMap
-        (pr', rMap) <- declareStateChannel pr tD boundNames stateMap
+        (pl', lMap) <- declareStateChannel pl toDeclare boundNames stateMap
+        (pr', rMap) <- declareStateChannel pr toDeclare boundNames stateMap
         return (ProcessComb a b pl' pr', M.union lMap rMap)
       ProcessAction (New var) an pr -> do
-        (pr', stateMap') <- declareStateChannel pr tD (var `S.insert` boundNames) stateMap
+        (pr', stateMap') <-  declareStateChannel pr toDeclare (var `S.insert` boundNames) stateMap
         return (ProcessAction (New var) an pr', stateMap')
 
       ProcessAction act an pr -> do
-        (pr', stateMap') <- declareStateChannel pr tD boundNames stateMap
+        (pr', stateMap') <- declareStateChannel pr toDeclare boundNames stateMap
         return (ProcessAction act an pr', stateMap')
+  else do
+    (newvars, newMap) <- newStates p declarables [] stateMap
+    (p', newMap')<- declareStateChannel p undeclarables boundNames newMap
+    return (addNews p' newvars, newMap')
+      where addNews pr [] = pr
+            addNews pr (v:d) = ProcessAction (New v) mempty (addNews pr d)
 
+newStates :: MonadFresh m => PlainProcess -> [SapicTerm] -> [SapicLVar]
+  -> StateMap -> m ([SapicLVar], StateMap)
+newStates _ [] declared stateMap = return (declared, stateMap)
+newStates p (v:declarables) declared stateMap = do
+    newvar <-  freshLVar "StateChannel" LSortMsg
+    let  newslvar = SapicLVar newvar (Just "channel")
+    let newMap =  M.insert v newslvar stateMap
+    newStates p declarables (newslvar:declared) newMap
 
 -- a state channel such that, 1) there is a single insert outside of a lock (this is the state initialisation); 2) every occurence of the state channel is either lock t; lookup t or insert t; unlock t.
 --getPureStates p currentPures oneOutside =
