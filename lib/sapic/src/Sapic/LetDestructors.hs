6d3ff6b1df31049403f7dd057a25e9e6a6dca624
Charlie Jacomme <jacomme@crans.org>
2021-05-25 15:48:50 +0200

    Fix let subtitution -> substitute in place when not matching

--- lib/sapic/src/Sapic/LetDestructors.hs
+++ lib/sapic/src/Sapic/LetDestructors.hs
@@ -42,7 +42,7 @@ mapProc rules (ProcessComb c@(Let t1 t2 mv) _ pl pr) =
       -- we are in the case where the let binding is of the form let invar = dest(rightTerms) in
       (case  L.foldl (findRule funsym) Nothing rules of
         -- if the desrtructor does not have any associated rule, we simply substitute in the process, to optimize. TODO -> should it be possible to declare a destructor without an equation ?
-        Nothing  | svar `S.member` mv -> do
+        Nothing  | not(svar `S.member` mv) -> do
             res <- applyM (substFromList (L.map (\x -> (x,t2)) (make_untyped_variant svar))) pl
             npl <- mapProc rules res
             return npl
@@ -70,7 +70,7 @@ mapProc rules (ProcessComb c@(Let t1 t2 mv) _ pl pr) =
                 subst = substFromList [(outvar, t1')]
                 new_an = annDestructorEquation leftermssubst (toPairs rightterms) elsebranch
           )
-    ( (LIT (Var svar)) , _ , _ )  | svar `S.member` mv -> do
+    ( (LIT (Var svar)) , _ , _ )  | not(svar `S.member` mv) -> do
             res <- applyM (substFromList  (L.map (\x -> (x,t2)) (make_untyped_variant svar))) pl
             npl <- mapProc rules res
             return npl
