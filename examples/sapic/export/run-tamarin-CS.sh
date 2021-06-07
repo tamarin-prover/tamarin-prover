02ef3924736803eb4fb29877df975550c25c283b
Charlie Jacomme <jacomme@crans.org>
2021-06-07 15:44:11 +0200

    Update case study script runner

--- examples/sapic/export/run-tamarin-CS.sh
+++ examples/sapic/export/run-tamarin-CS.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-SAPIC_CASE_STUDIES_SLOW=*.spthy
-
-
+filenames=(
+"SSH/ssh-with-one-forwarding.spthy"
+"SSH/ssh-without-forwarding.spthy"
+# "SSH/ssh-with-forwarding-bounded.spthy"  # this one has too many partial deconstructions
+"privacypass.spthy"
+"ExistingSapicModels/OTP.spthy"
+"ExistingSapicModels/nsl-no_as-untagged.spthy"
+"ExistingSapicModels/AC_sid_with_attack.spthy"
+"ExistingSapicModels/AKE.spthy"
+"ExistingSapicModels/AC.spthy"
+"ExistingSapicModels/AC_counter_with_attack.spthy"
+"States/scytl-voting-system.spthy"
+"States/secure-device.spthy"
+# "States/canauth.spthy" # not working on tamarin
+"LAKE/lake-edhoc.spthy"
+"LAKE/lake-edhoc-DHmode-KCI.spthy"
+"LAKE/lake-edhoc-DHmode-FS.spthy"
+"KEMTLS/kemtls.spthy"
+"KEMTLS/kemtls-noaead.spthy"
+"KEMTLS/kemtls-clientauth.spthy"
+)
 
 runners=("tamarin-prover")
 
@@ -26,13 +44,14 @@ for runner in "${runners[@]}"; do
     echo -n ", $runner result , $runner time"   >> "$outfilename"
 done
 echo ""  >> "$outfilename" # jump line
-# for file in $files; do
-find . -name "*.spthy"  | while read line; do
-    filename="$line"
+for file in "${filenames[@]}"; do
+# find . -name "*.spthy"  | while read line; do
+     #    filename="$line"
+     filename="$file"
     echo 'Extracting examples from '"$filename"
     echo -n "$filename,"  >> "$outfilename"
     for runner in "${runners[@]}"; do
 		exec_runner
     done
-   echo ""  >> "$outfilename" # jump line
+    echo ""  >> "$outfilename" # jump line
 done
