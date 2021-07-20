faa7649fec46b5bb438d76177307a3e13c5e2586
Jannik Dreier <jannik.dreier@loria.fr>
2021-07-20 10:53:28 +0200

    bumped LTS resolver version, fixed some compile warnings

--- lib/term/src/Term/Builtin/Rules.hs
+++ lib/term/src/Term/Builtin/Rules.hs
@@ -98,6 +98,7 @@ signatureRules = S.fromList [ verify (sign (x1,x2), x1, pk x2) `CtxtStRule` (StR
 revealSignatureRules = S.fromList [ revealVerify (revealSign (x1,x2), x1, pk x2) `CtxtStRule` (StRhs [[0,0]] trueC),
                                     extractMessage (revealSign (x1,x2)) `CtxtStRule` (StRhs [[0,0]] x1)]
 
+locationReportRules :: Set (CtxtStRule)
 locationReportRules = S.fromList [ check_rep (rep (x1,x2), x2) `CtxtStRule` (StRhs [[0,0]] x1),
                                    get_rep (rep (x1,x2)) `CtxtStRule` (StRhs [[0,0]] x1)
                                  ]
