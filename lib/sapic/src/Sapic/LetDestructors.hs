39e97e289d4c98397b4633d5f20336a56e536844
Charlie Jacomme <jacomme@crans.org>
2021-07-28 14:53:46 +0200

    Proverif export: a let binding over a destructor without equation always fails

--- lib/sapic/src/Sapic/LetDestructors.hs
+++ lib/sapic/src/Sapic/LetDestructors.hs
@@ -38,18 +38,11 @@ mapProc rules (ProcessAction ac ann p') = do
 
 mapProc rules (ProcessComb c@(Let t1 t2 mv) _ pl pr) =
   case (t1, viewTerm t1', viewTerm t2') of
-    ( (LIT (Var svar)) ,(Lit (Var _)), FApp funsym@(NoEq (_, (_,_,Destructor))) rightterms) ->
+    ( (LIT (Var _)) ,(Lit (Var _)), FApp funsym@(NoEq (_, (_,_,Destructor))) rightterms) ->
       -- we are in the case where the let binding is of the form let invar = dest(rightTerms) in
       (case  L.foldl (findRule funsym) Nothing rules of
-        -- if the desrtructor does not have any associated rule, we simply substitute in the process, to optimize. TODO -> should it be possible to declare a destructor without an equation ?
-        Nothing  | not(svar `S.member` mv) -> do
-            res <- applyM (substFromList (L.map (\x -> (x,t2)) (make_untyped_variant svar))) pl
-            npl <- mapProc rules res
-            return npl
-        Nothing -> do
-          npl <- mapProc rules pl
-          npr <- mapProc rules pr
-          return $ ProcessComb c (annElse elsebranch)  npl npr
+        -- if the desrtructor does not have any associated rule, it never succeed, and we thus always go in the else branch we simply substitute in the process, to optimize. TODO -> should it be possible to declare a destructor without an equation ?
+        Nothing -> mapProc rules pr
 --          ProcessComb c (annElse elsebranch)  (mapProc rules pl) (mapProc rules pr)
         Just  (leftterms, outvar) -> do
           -- TODO we should handle fresh vars here
