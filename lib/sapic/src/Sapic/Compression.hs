d2434d8c23ca9ef9fb81e8faf9f4978fbeb1bd17
Robert Künnemann <robert.kuennemann@cispa.saarland>
2021-07-28 10:36:39 +0200

    Hlint simplifications.

--- lib/sapic/src/Sapic/Compression.hs
+++ lib/sapic/src/Sapic/Compression.hs
@@ -46,23 +46,17 @@ isStateFact _ = False
 isStateProcessFact  :: Fact LNTerm -> Bool
 isStateProcessFact f = isStateFact f || isLetFact f
 
-isFreshFact :: Fact LNTerm -> Bool
-isFreshFact (Fact FreshFact _ _ ) = True
-isFreshFact _ = False
-
 sameName :: Fact LNTerm ->  Fact LNTerm -> Bool
 sameName  (Fact (ProtoFact _ name _) _ _)  (Fact (ProtoFact _ name2 _) _ _) = name==name2
 sameName _ _ = False
 
 -- get all rules with premice the given fact
 getPremRules:: Fact LNTerm ->  [Rule ProtoRuleEInfo] -> ([Rule ProtoRuleEInfo],[Rule ProtoRuleEInfo])
-getPremRules fact rules =
-  List.partition  (\x -> List.any (sameName fact) (L.get rPrems x)) rules
+getPremRules fact = List.partition  (List.any (sameName fact) . L.get rPrems)
 
 -- get all rules producing the given fact
 getConcsRules:: Fact LNTerm ->  [Rule ProtoRuleEInfo] -> ([Rule ProtoRuleEInfo],[Rule ProtoRuleEInfo])
-getConcsRules fact rules =
-  List.partition  (\x -> List.any (sameName fact) (L.get rConcs x)) rules
+getConcsRules fact = List.partition  (List.any (sameName fact) . L.get rConcs)
 
 -- Get the list of all state facts produced by a rule
 getProducedFacts :: [Rule ProtoRuleEInfo] -> S.Set (Fact LNTerm)
@@ -70,7 +64,7 @@ getProducedFacts rules =
   facts
   where
     facts = List.foldl (\acc (Rule _ _ rconc _ _) ->
-                              List.foldl (\set y -> y `S.insert` set) acc (List.filter isStateProcessFact rconc)
+                              List.foldl (flip S.insert) acc (List.filter isStateProcessFact rconc)
                            ) S.empty rules
 
 -- TODO : how to merge the info about a rule
@@ -86,12 +80,12 @@ mergeInfo info info2 =
 
 canMerge  :: Rule ProtoRuleEInfo -> Rule ProtoRuleEInfo -> Bool
 canMerge r1 r2
-  | ((ract /= []) && (ract2 /= [])) = False  -- we cannot merge rules if it makes events be simulataneous
+  | (ract /= []) && (ract2 /= []) = False  -- we cannot merge rules if it makes events be simulataneous
   | (List.length rprem2 > 1) && (List.length rconc >1) = False   -- we cannot merge rules if we are breaking asynchronous behavior (i.e u->v,w and w,r->t cannot be compress, as r might be produced byi
   | (List.length rconc > 1) && (ract2 /= []) = False   -- we cannot merge rules if we are breaking asynchronous behavior (i.e u->v,w, and v-E->t cannot be compressed, else an event that could have happened with w before E cannot do so anymore.
-  | (List.any isOutFact rconc) && (List.any isOutFact rconc2) = False -- we cannot merge rules if two Out become simultaneous (might break the fact that the attacker can know smth and not smth else at a timepoint
-  | (List.any isLetFact rconc) ||  (List.any isLetFact rprem)   = False -- we cannot merge rules when we are performing a let pattern matching
-  | (List.any isOutFact rconc) && (ract2 /= []) = False -- we cannot merge rules if a Out and an event become simultaneous (might break the fact that the attacker can know smth and not smth else at a timepoint
+  | List.any isOutFact rconc && List.any isOutFact rconc2 = False -- we cannot merge rules if two Out become simultaneous (might break the fact that the attacker can know smth and not smth else at a timepoint
+  | List.any isLetFact rconc ||  List.any isLetFact rprem   = False -- we cannot merge rules when we are performing a let pattern matching
+  | List.any isOutFact rconc && (ract2 /= []) = False -- we cannot merge rules if a Out and an event become simultaneous (might break the fact that the attacker can know smth and not smth else at a timepoint
   |otherwise = True
   where Rule _ rprem rconc ract _ = r1
         Rule _ rprem2 rconc2 ract2 _ = r2
@@ -100,13 +94,13 @@ canMerge r1 r2
 merge:: Rule ProtoRuleEInfo -> Rule ProtoRuleEInfo -> S.Set (Rule ProtoRuleEInfo) ->S.Set (Rule ProtoRuleEInfo)
 merge rule1 rule2 ruleset =
   if canMerge rule1 rule2 then
-    (Rule (mergeInfo rinfo rinfo2) newprem newrconc (ract++ract2) (rnew++rnew2)) `S.insert` ruleset
+    Rule (mergeInfo rinfo rinfo2) newprem newrconc (ract++ract2) (rnew++rnew2) `S.insert` ruleset
   else
     rule1 `S.insert` (rule2 `S.insert` ruleset)
   where Rule rinfo rprem rconc ract rnew = rule1
         Rule rinfo2 rprem2 rconc2 ract2 rnew2 = rule2
-        newprem = rprem ++ (List.filter (\x -> not(List.elem x rconc)) rprem2)
-        newrconc = rconc2 ++ (List.filter (\x -> not(List.elem x rprem2)) rconc)
+        newprem = rprem ++ List.filter (`notElem` rconc) rprem2
+        newrconc = rconc2 ++ List.filter (`notElem` rprem2) rconc
 
 -- Given two set of rules, such that the leftrules all produce a state (the same) consumed by the right rules, try to compress rules for each possible pairing between rules in leftrules and rightrules
 mergeRules::  [Rule ProtoRuleEInfo] -> [Rule ProtoRuleEInfo] -> [Rule ProtoRuleEInfo]
@@ -115,7 +109,7 @@ mergeRules leftrules rightrules =
     S.toList rulesset
   else
     leftrules ++ rightrules
-  where rulesset = List.foldl (\set l -> List.foldl (\set2 r-> merge l r set2) set rightrules) S.empty leftrules
+  where rulesset = List.foldl (\set l -> List.foldl (flip (merge l)) set rightrules) S.empty leftrules
 
 
 -- simpR ( Rule (ProtoRuleEInfo (StandRule name) _ _) rprem rconc ract rnew) =  ( Rule (ProtoRuleEInfo (StandRule name) [] []) rprem rconc ract rnew)
@@ -138,8 +132,8 @@ compress (fact:remainder) compressed_facts msr =
   where (new_msr,new_facts) = compressOne fact msr
         new_compressed_facts = fact `S.insert` compressed_facts
         new_facts_no_compress = new_facts S.\\ new_compressed_facts
-        new_facts_no_remainder = new_facts_no_compress S.\\ (S.fromList remainder)
-        new_facts_remainder = ((S.toList new_facts_no_remainder)++remainder)   -- we avoid duplicates between remainder and newfactsnocompress
+        new_facts_no_remainder = new_facts_no_compress S.\\ S.fromList remainder
+        new_facts_remainder = S.toList new_facts_no_remainder++remainder   -- we avoid duplicates between remainder and newfactsnocompress
 
 
 -- Start the compression by the init fact introduced by the translation
@@ -150,4 +144,4 @@ pathCompression msr =
   where initfact = Sapic.Facts.factToFact (State LState [] S.empty)
          -- in the end, we remove the useless dangling rules
         filterDeadend rs = List.filter (\  (Rule _ _ rconc ract _) ->
-                                          not(ract == [] && rconc == [])  ) rs
+                                          not(null ract && null rconc)  ) rs
