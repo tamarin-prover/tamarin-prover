851fad04a84f16c7b8d6b5b06c5424bbe79f838e
Philip <73644534+PhilipLukertWork@users.noreply.github.com>
2021-10-20 10:54:41 +0200

    Contradictions.hs

--- lib/theory/src/Theory/Constraint/Solver/Contradictions.hs
+++ lib/theory/src/Theory/Constraint/Solver/Contradictions.hs
@@ -62,6 +62,7 @@ import           Term.Rewriting.Norm            (maybeNotNfSubterms, nf')
 -- | Reasons why a constraint 'System' can be contradictory.
 data Contradiction =
     Cyclic                         -- ^ The paths are cyclic.
+  | SubtermCyclic                  -- ^ The subterm predicates form a cycle
   | NonNormalTerms                 -- ^ Has terms that are not in normal form.
   -- | NonLastNode                    -- ^ Has a non-silent node after the last node.
   | ForbiddenExp                   -- ^ Forbidden Exp-down rule instance
@@ -89,23 +90,25 @@ contradictorySystem ctxt = not . null . contradictions ctxt
 contradictions :: ProofContext -> System -> [Contradiction]
 contradictions ctxt sys = F.asum
     -- CR-rule **
-    [ guard (D.cyclic $ rawLessRel sys)             *> pure Cyclic
+    [ guard (D.cyclic $ rawLessRel sys)                            *> pure Cyclic
+    -- CR-rule *S_Subterm-Chain-Fail*
+    , guard (hasSubtermCycle reducible subtermRel) *> pure SubtermCyclic
     -- CR-rule *N1*
-    , guard (hasNonNormalTerms sig sys)             *> pure NonNormalTerms
+    , guard (hasNonNormalTerms sig sys)                            *> pure NonNormalTerms
     -- FIXME: add CR-rule
-    , guard (hasForbiddenKD sys)                    *> pure ForbiddenKD
+    , guard (hasForbiddenKD sys)                                   *> pure ForbiddenKD
     -- FIXME: add CR-rule
-    , guard (hasImpossibleChain ctxt sys)           *> pure ImpossibleChain
+    , guard (hasImpossibleChain ctxt sys)                          *> pure ImpossibleChain
     -- CR-rule *N7*
-    , guard (enableDH msig && hasForbiddenExp sys)  *> pure ForbiddenExp
+    , guard (enableDH msig && hasForbiddenExp sys)                 *> pure ForbiddenExp
     -- FIXME: add CR-rule
-    , guard (enableBP msig && hasForbiddenBP sys)   *> pure ForbiddenBP
+    , guard (enableBP msig && hasForbiddenBP sys)                  *> pure ForbiddenBP
     -- New CR-Rule *N6'*
-    , guard (hasForbiddenChain sys)                 *> pure ForbiddenChain
+    , guard (hasForbiddenChain sys)                                *> pure ForbiddenChain
     -- CR-rules *S_≐* and *S_≈* are implemented via the equation store
-    , guard (eqsIsFalse $ L.get sEqStore sys)       *> pure IncompatibleEqs
+    , guard (eqsIsFalse $ L.get sEqStore sys)                      *> pure IncompatibleEqs
     -- CR-rules *S_⟂*, *S_{¬,last,1}*, *S_{¬,≐}*, *S_{¬,≈}*
-    , guard (S.member gfalse $ L.get sFormulas sys) *> pure FormulasFalse
+    , guard (S.member gfalse $ L.get sFormulas sys)                *> pure FormulasFalse
     ]
     ++
     -- This rule is not yet documented. It removes constraint systems that
@@ -119,6 +122,8 @@ contradictions ctxt sys = F.asum
   where
     sig  = L.get pcSignature ctxt
     msig = mhMaudeSig . L.get pcMaudeHandle $ ctxt
+    subtermRel = rawSubtermRel $ L.get sEqStore sys  --TODO-BIG: adapt this to not take the rel from the Eqstore
+    reducible = reducibleFunSyms msig
 
 -- | New normal form condition:
 -- We do not allow @KD(t)@ facts if @t@ does not contain
@@ -428,6 +433,7 @@ isForbiddenDEMapOrder sys (i, ruDEMap) = fromMaybe False $ do
 prettyContradiction :: Document d => Contradiction -> d
 prettyContradiction contra = case contra of
     Cyclic                       -> text "cyclic"
+    SubtermCyclic                -> text "subterm cyclic"
     IncompatibleEqs              -> text "incompatible equalities"
     NonNormalTerms               -> text "non-normal terms"
     ForbiddenExp                 -> text "non-normal exponentiation rule instance"
