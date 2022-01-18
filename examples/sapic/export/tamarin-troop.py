ce4bff23f2928398735c440b77c675a478747ef2
Niklas Medinger <s8nimedi@stud.uni-saarland.de>
2022-01-18 17:02:43 +0100

    Finished job building for Deepsec/ProVerif. TODO: Tamarin + execution

--- examples/sapic/export/tamarin-troop.py
+++ examples/sapic/export/tamarin-troop.py
@@ -3,6 +3,7 @@
 import argparse
 import subprocess
 import os
+import itertools
 
 # Tamarins heustics
 HEURISTICS = [ 's', 'S', 'c', 'C', 'i', 'I' ]
@@ -13,6 +14,16 @@ TAMARIN_COMMAND = "tamarin-prover-proverif-output"
 # Dict mapping tool to file type
 TOOL_TO_FILE_TYPE = { "spthy": ".spthy", "proverif": ".pv", "deepsec": ".dps" }
 
+def intermediate_file_name(input_file, tool, lemma):
+    """
+        This function generates a file name for the intermediate generated
+        for deepsec/proverif.
+        TODO: Once we can generate files on a per lemma basis, we need to
+        include the name of the lemma in the file name as well.
+    """
+    # Get input_file without '.spthy'
+    file_name, _, _ = input_file.partition('.')
+    return file_name + "_" + tool + TOOL_TO_FILE_TYPE[tool]
 
 def generate_files(input_file, flags, lemmas, argdict, diff):
     """
@@ -26,11 +37,9 @@ def generate_files(input_file, flags, lemmas, argdict, diff):
     # Diff mode?
     diffstring = " --diff" if diff else ""
 
-    # Get input_file without '.spthy'
-    file_name, _, _ = input_file.partition('.')
 
-    for key in argdict.keys():
-        if key == SPTHY:
+    for tool in argdict.keys():
+        if tool == SPTHY:
             # Skip Tamarin/spthy.
             continue
 
@@ -44,22 +53,71 @@ def generate_files(input_file, flags, lemmas, argdict, diff):
         # For now, we can only prove files that contain a single lemma?!
         # Once --lemma has been changed, we need to change the call to Tamarin
         # to include this parameter!
-
         # Build command
-        cmd = " ".join([TAMARIN_COMMAND, '-m='+key] + flags + [input_file])
+        cmd = " ".join([TAMARIN_COMMAND, '-m='+tool] + flags + [input_file])
         # Add diff flag
         cmd = cmd + diffstring
         print(cmd)
         # Change file type according to current tool
-        destination = " > " + file_name + "_" + key + TOOL_TO_FILE_TYPE[key]
+        # TODO: Add lemma parameter to file name
+        destination = " > " + intermediate_file_name(input_file, tool, "")
         # Concatenate the cmd and destination, and run it
         subprocess.run(cmd + destination, shell=True, check=True)
+
+def generate_jobs(input_file, lemmas, argdict):
+    """
+        This function generates the jobs that we want to concurrently execute.
+        A job is a list of arguments that, when used by a Popen call,
+        correspond to a call to Tamarin/Deepsec/Proverif on a file/lemma we
+        want to time.
+
+        For instance:
+            [ proverif, example.pv, -test]
+        is a valid job. Another example:
+            [ deepsec, example.dps, --local-workers 12]
+        is also a valid job.
+    """
+    jobs = []
+    for tool in argdict.keys():
+        # Generate jobs for each tool
+        if tool == SPTHY:
+            # Special case for Tamarin. We do not generate a file per lemma
+            # for Tamarin. Instead, we need to use its CLI/lemma selector.
+            # Thus, generating jobs for Tamarin is different than generating
+            # jobs for ProVerif/Deepsec.
+            jobs += generate_tamarin_jobs()
+
+        # TODO: For debugging/simplification we convert the iterable object
+        # other_jobs into a list here. This might not be the most efficient
+        # way. Revisit this at a later point.
+        jobs += list(generate_non_tamarin_jobs(tool,
+                                               input_file, lemmas, argdict))
+    return jobs
+
+def generate_non_tamarin_jobs(tool, input_file, lemmas, argdict):
+    """
+        Returns a list of jobs (list of lists).
+    """
+    jobs = []
+    for lemma in lemmas:
+        print(tool, input_file, lemma, argdict[tool])
+        # Build the crossproduct of tool, input_file, CLI parameters
+        # * is the unpack operator. It extracts all items in the list
+        # argdict[tool] and adds them to the function call.
+        if not argdict[tool][0]:
+            # If there were no CLI params specified.
+            jobs += list(itertools.product([tool]
+                   ,[intermediate_file_name(input_file, tool, lemma)]))
+        else:
+            # If CLI params were specified, we use them.
+            jobs += list(itertools.product([tool]
+                   ,[intermediate_file_name(input_file, tool, lemma)]
+                   ,*argdict[tool]))
+    return jobs
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # We assume that we can pass the lemmas to Tamarin, and Tamarin will
-    # create a file for each lemma. However, currently this is not implemented.
     parser.add_argument('-l', '--lemma', action='extend', nargs='+', type=str,
                         help='prove the given lemmas')
     # Flags for Tamarin's preprocessor. Used during file/model generation
@@ -69,17 +127,17 @@ if __name__ == '__main__':
     parser.add_argument('input_file', type=str, help='the sapic file')
     # Args for ProVerif.
     parser.add_argument('-p', '--proverif', action='append',
-                        nargs='+', type=str, help='arguments for ProVerif')
+                        nargs='*', type=str, help='arguments for ProVerif')
     # Args for Tamarin
     parser.add_argument('-t', '--tamarin', action='append',
-                        nargs='+', type=str, help='arguments for Tamarin')
+                        nargs='*', type=str, help='arguments for Tamarin')
     # Heuristics for Tamarin
     parser.add_argument('-H', '--heuristic', action='extend',
                         nargs='+', type=str, choices=HEURISTICS,
                         help='heuristics Tamarin should use')
     # Args for Deepsec
     parser.add_argument('-d', '--deepsec', action='append',
-                        nargs='+', type=str, help='arguments for Deepsec')
+                        nargs='*', type=str, help='arguments for Deepsec')
     # Arg for Tamarin diff mode
     parser.add_argument('--diff', action='store_true',
                         help="Flag to use Tamarin's diff mode\
@@ -106,12 +164,12 @@ if __name__ == '__main__':
             argdict[SPTHY] = args.heuristic
 
     # Generate desired model files from the input file
-    generate_files(args.input_file, flags, lemmas, argdict, args.diff)
-    print(lemmas)
-    print(flags)
-    print(args.diff)
-    print(argdict)
+    # generate_files(args.input_file, flags, lemmas, argdict, args.diff)
+    # print(lemmas)
+    # print(flags)
+    # print(args.diff)
+    # print(argdict)
 
-    # Concurrently execute the cross-product of the given args
-    # on the generate model files
-    pass
+    # Generate the jobs that we want to concurrently execute
+    jobs = generate_jobs(args.input_file, lemmas, argdict)
+    print(jobs)
