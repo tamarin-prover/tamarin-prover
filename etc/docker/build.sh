417feec37cc7f7d448b8ec4f8c6d79ddb4214b13
Robert Künnemann <robert.kuennemann@cispa.saarland>
2022-01-25 12:23:55 +0100

    WIP docker adaptation.

--- etc/docker/build.sh
+++ etc/docker/build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # call in top-level dir...
-docker build -t tamarin-prover/tamarin:latest -f etc/docker/Dockerfile .
-docker build -t tamarin-prover/protocolplatform:latest -f etc/docker/Dockerfile-platform
+docker build -t protocolplatform/tamarin:latest -f etc/docker/Dockerfile .
