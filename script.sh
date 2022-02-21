8184943e88caa87888d137a9beef6c707db27707
mracouch <maiwenn.racouchot@inria.fr>
2022-02-21 17:30:18 +0100

    Ajout de l'option oracle aux scripts

--- script.sh
+++ script.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-#Format: ./script spthyFile lemmaFiles
+#Format: ./script spthyFile lemmaFiles oracleFile
 
 theoryFile=$1
 theoryName=benchmark/$(sed 's#.*/\([^\.]*\).*#\1#' <<<$theoryFile).txt
 lemmaFilename=$2
 
 while read line; do
-	if [ ${line:0:1} != '#' ] ; then
-		echo $line >> $theoryName.txt
-		{ time tamarin-prover --prove=$line $theoryFile > benchmark/$line.txt 2>&1 ; } 2>> $theoryName.txt
+	if [ $# -eq 3 ] ; then 
+		if [ ${line:0:1} != '#' ] ; then
+			echo $line >> $theoryName.txt
+			echo "oracle" >> $theoryName.txt
+			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile > benchmark/$line.txt 2>&1 ; } 2>> $theoryName.txt
+		fi
+	else
+		if [ ${line:0:1} != '#' ] ; then
+			echo $line >> $theoryName.txt
+			echo "tactic" >> $theoryName.txt
+			{ time tamarin-prover --prove=$line $theoryFile > benchmark/$line.txt 2>&1 ; } 2>> $theoryName.txt
+		fi
 	fi
 done < $lemmaFilename
