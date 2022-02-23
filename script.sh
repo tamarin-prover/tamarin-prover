97bca343908e8568d8e742b85443409a736c36f8
mracouch <maiwenn.racouchot@inria.fr>
2022-02-23 16:39:22 +0100

    Version correcte oracle oracle_correctness_two_valid_linked_certs

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Format: ./script spthyFile lemmaFiles oracleFile
+
+theoryFile=$1
+theoryName=benchmark/$(sed 's#.*/\([^\.]*\).*#\1#' <<<$theoryFile).txt
+lemmaFilename=$2
+
+while read line; do
+	if [ $# -eq 3 ] ; then 
+		if [ ${line:0:1} != '#' ] ; then
+			echo $line >> $theoryName.txt
+			echo "oracle" >> $theoryName.txt
+			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile +RTS -N10 -RTS  > benchmark/$line.txt 2>&1 ; } 2>> $theoryName
+		fi
+	else
+		if [ ${line:0:1} != '#' ] ; then
+			echo $line >> $theoryName.txt
+			echo "tactic" >> $theoryName.txt
+			{ time tamarin-prover --prove=$line $theoryFile +RTS -N10 -RTS  > benchmark/$line.txt 2>&1 ; } 2>> $theoryName
+		fi
+	fi
+done < $lemmaFilename
