735acd351310834980162a74da1be97ab90b5852
mracouch <maiwenn.racouchot@inria.fr>
2022-02-23 10:48:12 +0100

    Modif pour serveur

--- script.sh
+++ script.sh
@@ -9,15 +9,13 @@ lemmaFilename=$2
 while read line; do
 	if [ $# -eq 3 ] ; then 
 		if [ ${line:0:1} != '#' ] ; then
-			echo $line >> $theoryName.txt
-			echo "oracle" >> $theoryName.txt
-			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile > benchmark/$line.txt 2>&1 ; } 2>> $theoryName.txt
+			echo $line >> oracle_$theoryName.txt
+			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile > benchmark/$line.txt 2>&1 ; } 2>> oracle_$theoryName.txt
 		fi
 	else
 		if [ ${line:0:1} != '#' ] ; then
-			echo $line >> $theoryName.txt
-			echo "tactic" >> $theoryName.txt
-			{ time tamarin-prover --prove=$line $theoryFile > benchmark/$line.txt 2>&1 ; } 2>> $theoryName.txt
+			echo $line >> tactic_$theoryName.txt
+			{ time tamarin-prover --prove=$line $theoryFile > benchmark/$line.txt 2>&1 ; } 2>> tactic_$theoryName.txt
 		fi
 	fi
 done < $lemmaFilename
