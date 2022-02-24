a79aef3cb98942d0f2fcbe2537cdfeac03717a09
mracouch <maiwenn.racouchot@inria.fr>
2022-02-24 09:41:28 +0100

    script

--- script.sh
+++ script.sh
@@ -9,14 +9,14 @@ lemmaFilename=$2
 while read line; do
 	if [ $# -eq 3 ] ; then 
 		if [ ${line:0:1} != '#' ] ; then
-			echo $line >> $theoryName.txt
-			echo "oracle" >> $theoryName.txt
+			echo $line >> $theoryName
+			echo "oracle" >> $theoryName
 			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile +RTS -N10 -RTS  > benchmark/$line.txt 2>&1 ; } 2>> $theoryName
 		fi
 	else
 		if [ ${line:0:1} != '#' ] ; then
-			echo $line >> $theoryName.txt
-			echo "tactic" >> $theoryName.txt
+			echo $line >> $theoryName
+			echo "tactic" >> $theoryName
 			{ time tamarin-prover --prove=$line $theoryFile +RTS -N10 -RTS  > benchmark/$line.txt 2>&1 ; } 2>> $theoryName
 		fi
 	fi
