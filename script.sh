420dddca45bc7ff5a51b9352d5d12a2a5827ae89
mracouch <maiwenn.racouchot@inria.fr>
2022-02-24 14:42:39 +0100

    Matériel pour Elise

--- script.sh
+++ script.sh
@@ -11,13 +11,13 @@ while read line; do
 		if [ ${line:0:1} != '#' ] ; then
 			echo $line >> $theoryName
 			echo "oracle" >> $theoryName
-			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile +RTS -N10 -RTS  > benchmark/$line.txt 2>&1 ; } 2>> $theoryName
+			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile +RTS -N10 -RTS  > benchmark/oracle_$line.txt 2>&1 ; } 2>> $theoryName
 		fi
 	else
 		if [ ${line:0:1} != '#' ] ; then
 			echo $line >> $theoryName
 			echo "tactic" >> $theoryName
-			{ time tamarin-prover --prove=$line $theoryFile +RTS -N10 -RTS  > benchmark/$line.txt 2>&1 ; } 2>> $theoryName
+			{ time tamarin-prover --prove=$line $theoryFile +RTS -N10 -RTS  > benchmark/tactic_$line.txt 2>&1 ; } 2>> $theoryName
 		fi
 	fi
 done < $lemmaFilename
