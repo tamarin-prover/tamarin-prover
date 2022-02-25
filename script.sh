8147dcc0ee5d73228de32a71cb28df2bec27f3b9
mracouch <maiwenn.racouchot@inria.fr>
2022-02-25 14:09:43 +0100

    Nouvelle version du script et des inputs

--- script.sh
+++ script.sh
@@ -3,21 +3,40 @@
 #Format: ./script spthyFile lemmaFiles oracleFile
 
 theoryFile=$1
-theoryName=benchmark/$(sed 's#.*/\([^\.]*\).*#\1#' <<<$theoryFile).txt
+theoryName=$(sed 's#.*/\([^\.]*\).*#\1#' <<<$theoryFile)
 lemmaFilename=$2
 
-while read line; do
-	if [ $# -eq 3 ] ; then 
-		if [ ${line:0:1} != '#' ] ; then
-			echo $line >> $theoryName
-			echo "oracle" >> $theoryName
-			{ time tamarin-prover --prove=$line --heuristic=o --oraclename=$3 $theoryFile +RTS -N10 -RTS  > benchmark/oracle_$line.txt 2>&1 ; } 2>> $theoryName
+cd benchmark
+
+if [ ! -d $theoryName ]; then 
+	mkdir $theoryName
+fi
+cd $theoryName
+
+cpt=0
+timeFile="time_"$cpt
+while [ -f $timeFile ]; do
+	cpt=$((cpt+1))
+	timeFile="time_"$cpt
+done
+
+for i in {0..9}; do 
+	echo "Iteration: "$i
+	while read line; do
+		if [ $# -eq 3 ] ; then 
+			if [ ${line:0:1} != '#' ] ; then
+				echo $line >> $timeFile
+				echo "oracle" >> $timeFile
+				{ time tamarin-prover --prove=$line --heuristic=o --oraclename=../../$3 ../../$theoryFile +RTS -N10 -RTS  > oracle_$line 2>&1 ; } 2>> $timeFile
+			fi
+		else
+			if [ ${line:0:1} != '#' ] ; then
+				echo $line >> $timeFile
+				echo "tactic" >> $timeFile
+				{ time tamarin-prover --prove=$line ../../$theoryFile +RTS -N10 -RTS  > tactic_$line 2>&1 ; } 2>> $timeFile
+			fi
 		fi
-	else
-		if [ ${line:0:1} != '#' ] ; then
-			echo $line >> $theoryName
-			echo "tactic" >> $theoryName
-			{ time tamarin-prover --prove=$line $theoryFile +RTS -N10 -RTS  > benchmark/tactic_$line.txt 2>&1 ; } 2>> $theoryName
-		fi
-	fi
-done < $lemmaFilename
+	done < ../../$lemmaFilename
+done
+
+cd ../..
