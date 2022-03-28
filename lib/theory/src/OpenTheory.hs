5e7b5610d082d0580da04a49c75cd030f95f9fc3
HongThai <s8holuuu@stud.uni-saarland.de>
2022-03-28 15:05:05 +0200

    Put data structures in SpecificProof.hs

--- lib/theory/src/OpenTheory.hs
+++ lib/theory/src/OpenTheory.hs
@@ -6,7 +6,7 @@
 
 module OpenTheory (
     module OpenTheory
-    
+    ,module SpecificProof
     ) where
 
 import GHC.Generics
@@ -36,38 +36,12 @@ import           Theory.Tools.IntruderRules
 import           Term.Positions
 
 import           Utils.Misc
-import Theories
 import Safe
 
-------------------------------------------------------------------------------
--- Specific proof types
-------------------------------------------------------------------------------
+import Theories
 
--- | Proof skeletons are used to represent proofs in open theories.
-type ProofSkeleton    = Proof ()
 
--- | Convert a proof skeleton to an incremental proof without any sequent
--- annotations.
-skeletonToIncrementalProof :: ProofSkeleton -> IncrementalProof
-skeletonToIncrementalProof = fmap (fmap (const Nothing))
-
--- | Convert an incremental proof to a proof skeleton by dropping all
--- annotations.
-incrementalToSkeletonProof :: IncrementalProof -> ProofSkeleton
-incrementalToSkeletonProof = fmap (fmap (const ()))
-
--- | Proof skeletons are used to represent proofs in open theories.
-type DiffProofSkeleton    = DiffProof ()
-
--- | Convert a proof skeleton to an incremental proof without any sequent
--- annotations.
-skeletonToIncrementalDiffProof :: DiffProofSkeleton -> IncrementalDiffProof
-skeletonToIncrementalDiffProof = fmap (fmap (const Nothing))
-
--- | Convert an incremental proof to a proof skeleton by dropping all
--- annotations.
-incrementalToSkeletonDiffProof :: IncrementalDiffProof -> DiffProofSkeleton
-incrementalToSkeletonDiffProof = fmap (fmap (const ()))
+import SpecificProof
 
 
 -- Lemma construction/modification
---
+++ lib/theory/src/SpecificProof.hs
@@ -0,0 +1,38 @@
+module SpecificProof (
+    module SpecificProof
+) where
+
+import           Prelude                             hiding (id, (.))
+
+import           Theory.Proof
+
+------------------------------------------------------------------------------
+-- Specific proof types
+------------------------------------------------------------------------------
+
+-- | Proof skeletons are used to represent proofs in open theories.
+type ProofSkeleton    = Proof ()
+
+-- | Convert a proof skeleton to an incremental proof without any sequent
+-- annotations.
+skeletonToIncrementalProof :: ProofSkeleton -> IncrementalProof
+skeletonToIncrementalProof = fmap (fmap (const Nothing))
+
+-- | Convert an incremental proof to a proof skeleton by dropping all
+-- annotations.
+incrementalToSkeletonProof :: IncrementalProof -> ProofSkeleton
+incrementalToSkeletonProof = fmap (fmap (const ()))
+
+-- | Proof skeletons are used to represent proofs in open theories.
+type DiffProofSkeleton    = DiffProof ()
+
+-- | Convert a proof skeleton to an incremental proof without any sequent
+-- annotations.
+skeletonToIncrementalDiffProof :: DiffProofSkeleton -> IncrementalDiffProof
+skeletonToIncrementalDiffProof = fmap (fmap (const Nothing))
+
+-- | Convert an incremental proof to a proof skeleton by dropping all
+-- annotations.
+incrementalToSkeletonDiffProof :: IncrementalDiffProof -> DiffProofSkeleton
+incrementalToSkeletonDiffProof = fmap (fmap (const ()))
+
