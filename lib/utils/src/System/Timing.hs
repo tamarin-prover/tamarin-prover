tamarin-prover/tamarin-prover

commit 7b60a75c517d68a3ba894700ebef20bdd36e038e

Refactor of TheoryLoader / Batch / Interactive (#484)
* Update GHC to 9.2.2

* Add HasField instances for lemma names

* Unify lemmaSelector and diffLemmaSelector

* Unify constructAutoProver and constructAutoDiffProver

* Remove IO monad from addMessageDeductionRulesVariants(Diff)

* Remove IO monad from close(Diff)ThyWithMaude

* Remove unused load theory functions

* Unify checking of arguments in TheoryLoader

* WIP: Refactoring of theory loading

* Refactoring of theory loading (batch mode)

* Fix pretty printing of translation items

* Refactoring of theory loading (interactive mode)

* Fix warnings

* Remove unused load theory functions

* Remove unused noteWellformedness functions

* Improve batch output when quit-on-warning is specified

* Use lenses and improve show instance of TheoryLoadError

* Use die instead of error

* Terminate web server when quit-on-warning enabled and error occurs

* Downgrade GHC to lts-19.16 (9.0.2)

* Fix warnings

* Add warning re wrong analysis results only in prove mode

src/Main/Mode/Batch.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 -- |
 -- Copyright   : (c) 2010, 2011 Benedikt Schmidt & Simon Meier
 -- License     : GPL v3 (see LICENSE)
@@ -11,10 +12,8 @@ module Main.Mode.Batch (
   ) where
 
 import           Control.Basics
-import           Control.DeepSeq                 (force)
-import           Control.Exception               (evaluate)
 import           Data.List
-import           Data.Maybe
+import           Data.Bitraversable              (bisequence)
 import           System.Console.CmdArgs.Explicit as CmdArgs
 import           System.FilePath
 import           System.Timing                   (timed)
@@ -22,8 +21,7 @@ import           Extension.Data.Label
 
 import qualified Text.PrettyPrint.Class          as Pretty
 
-import           Theory
-import           Theory.Tools.Wellformedness     (checkWellformednessDiff)
+import           Theory hiding (closeTheory)
 
 import qualified Sapic
 import qualified Export
@@ -34,7 +32,9 @@ import           Main.TheoryLoader
 import           Main.Utils
 
 import           Theory.Module
--- import           Debug.Trace
+import           Control.Monad.Except (MonadIO(liftIO), runExceptT)
+import           System.Exit (die)
+import Theory.Tools.Wellformedness (prettyWfErrorReport)
 
 -- | Batch processing mode.
 batchMode :: TamarinMode
@@ -80,33 +80,67 @@ run :: TamarinMode -> Arguments -> IO ()
 run thisMode as
   | null inFiles = helpAndExit thisMode (Just "no input files given")
   | argExists "parseOnly" as || argExists "outModule" as = do
-      mapM_ processThy inFiles
+      res <- mapM processThy inFiles
+      let (thys, _) = unzip res
+
+      mapM_ (putStrLn . renderDoc) thys
       putStrLn ""
-  | otherwise  = do
+  | otherwise = do
       _ <- ensureMaude as
-      putStrLn ""
-      summaries <- mapM processThy inFiles
-      putStrLn ""
-      putStrLn $ replicate 78 '='
-      putStrLn "summary of summaries:"
-      putStrLn ""
-      putStrLn $ renderDoc $ Pretty.vcat $ intersperse (Pretty.text "") summaries
-      putStrLn ""
-      putStrLn $ replicate 78 '='
+      res <- mapM (timed . processThy) inFiles
+      let (thys, times) = unzip res
+      let (docs, reps) = unzip thys
+
+      if writeOutput then do
+        let maybeOutFiles = sequence $ mkOutPath <$> inFiles
+        outFiles <- case maybeOutFiles of
+          Just f -> return f
+          Nothing -> die "Please specify a valid output file/directory"
+        let repsWithInfo = ppRep <$> zip4 inFiles (Just <$> outFiles) times reps
+        let summary = Pretty.vcat $ intersperse (Pretty.text "") repsWithInfo
+
+        mapM_ (\(o, d) -> writeFileWithDirs o (renderDoc d)) (zip outFiles docs)
+        putStrLn $ renderDoc $ ppSummary summary
+      else do
+        let repsWithInfo = ppRep <$> zip4 inFiles (repeat Nothing) times reps
+        let summary = Pretty.vcat $ intersperse (Pretty.text "") repsWithInfo
+
+        mapM_ (putStrLn . renderDoc) docs
+        putStrLn $ renderDoc $ ppSummary summary
   where
+    ppSummary summary = Pretty.vcat [ Pretty.text $ ""
+                                    , Pretty.text $ replicate 78 '='
+                                    , Pretty.text $ "summary of summaries:"
+                                    , Pretty.text $ ""
+                                    , summary
+                                    , Pretty.text $ ""
+                                    , Pretty.text $ replicate 78 '=' ]
+
+    ppRep (inFile, outFile, time, summary)=
+      Pretty.vcat [ Pretty.text $ "analyzed: " ++ inFile
+                  , Pretty.text $ ""
+                  , Pretty.text $ ""
+                  , Pretty.nest 2 $ Pretty.vcat [
+                      maybe Pretty.emptyDoc (\o -> Pretty.text $ "output:          " ++ o) outFile
+                    , Pretty.text $ "processing time: " ++ show time
+                    , Pretty.text $ ""
+                    , summary ] ]
+
     -- handles to arguments
     -----------------------
     inFiles    = reverse $ findArg "inFile" as
 
+    thyLoadOptions = case mkTheoryLoadOptions as of
+      Left (ArgumentError e) -> error e
+      Right opts             -> opts
+
     -- output generation
     --------------------
-
-    dryRun = not (argExists "outFile" as || argExists "outDir" as)
+    writeOutput = argExists "outFile" as || argExists "outDir" as
 
     mkOutPath :: FilePath  -- ^ Input file name.
-              -> FilePath  -- ^ Output file name.
+              -> Maybe FilePath  -- ^ Output file name.
     mkOutPath inFile =
-        fromMaybe (error "please specify an output file or directory") $
             do outFile <- findArg "outFile" as
                guard (outFile /= "")
                return outFile
@@ -122,95 +156,46 @@ run thisMode as
 
     -- theory processing functions
     ------------------------------
+    processThy :: FilePath -> IO (Pretty.Doc, Pretty.Doc)
+    processThy inFile = either handleError return <=< runExceptT $ do
+      srcThy <- liftIO $ readFile inFile
+      thy    <- loadTheory thyLoadOptions srcThy inFile
 
-    processThy :: FilePath -> IO Pretty.Doc
-    processThy inFile
-      | argExists "parseOnly" as && argExists "diff" as =
-          out (const Pretty.emptyDoc) (return . prettyOpenDiffTheory) (loadOpenDiffThy   as inFile)
-      | argExists "parseOnly" as || argExists "outModule" as =
-          out (const Pretty.emptyDoc) choosePretty (loadOpenThy as inFile)
-      | argExists "diff" as =
-          out ppWfAndSummaryDiff      (return . prettyClosedDiffTheory) (loadClosedDiffThy as inFile)
-      | otherwise        = do
-          (thy,report) <- loadClosedThyWf as inFile
-          out (ppWfAndSummary report) (return . prettyClosedTheory) (return thy)
+      if isParseOnlyMode then do
+        either (\t -> bisequence (liftIO $ choosePretty t, return Pretty.emptyDoc))
+               (\d -> return (prettyOpenDiffTheory d, Pretty.emptyDoc)) thy
+      else do
+        let sig = either (get thySignature) (get diffThySignature) thy
+        sig'   <- liftIO $ toSignatureWithMaude (get oMaudePath thyLoadOptions) sig
+
+        (report, thy') <- closeTheory thyLoadOptions sig' thy
+        either (\t -> return (prettyClosedTheory t,     ppWf report Pretty.$--$ prettyClosedSummary t))
+               (\d -> return (prettyClosedDiffTheory d, ppWf report Pretty.$--$ prettyClosedDiffSummary d)) thy'
       where
-        ppAnalyzed = Pretty.text $ "analyzed: " ++ inFile
-        ppWfAndSummary report thy = do
-            report
-            Pretty.$--$ prettyClosedSummary thy
+        isParseOnlyMode = get oParseOnlyMode thyLoadOptions
 
-        ppWfAndSummaryDiff thy = do
-            reportWellformednessDoc $ checkWellformednessDiff (openDiffTheory thy) (get diffThySignature thy)
-            Pretty.$--$ prettyClosedDiffSummary thy
+        handleError (ParserError e) = error $ show e
+        handleError (WarningError report) = do
+          putStrLn $ renderDoc $ Pretty.vcat [ Pretty.text ""
+                                             , Pretty.text "WARNING: the following wellformedness checks failed!"
+                                             , Pretty.text ""
+                                             , prettyWfErrorReport report
+                                             , Pretty.text "" ]
+          die "quit-on-warning mode selected - aborting on wellformedness errors."
 
-        choosePretty = case getOutputModule as of
-          ModuleSpthy      -> return . prettyOpenTheory  <=< Sapic.warnings -- output as is, including SAPIC elements
-          ModuleSpthyTyped -> return . prettyOpenTheory <=< Sapic.typeTheory <=< Sapic.warnings  -- additionally type
-          ModuleMsr        -> return . prettyOpenTranslatedTheory
-            <=< (return . (filterLemma $ lemmaSelector as))
+        ppWf []  = Pretty.emptyDoc
+        ppWf rep = Pretty.vcat $ Pretty.text ("WARNING: " ++ show (length rep) ++ " wellformedness check failed!")
+                             : [ Pretty.text   "         The analysis results might be wrong!" | get oProveMode thyLoadOptions ]
+
+        choosePretty = case get oOutputModule thyLoadOptions of
+          Nothing               -> return . prettyOpenTheory  <=< Sapic.warnings -- output as is, including SAPIC elements
+          Just ModuleSpthy      -> return . prettyOpenTheory  <=< Sapic.warnings -- output as is, including SAPIC elements
+          Just ModuleSpthyTyped -> return . prettyOpenTheory <=< Sapic.typeTheory <=< Sapic.warnings  -- additionally type
+          Just ModuleMsr        -> return . prettyOpenTranslatedTheory
+            <=< (return . filterLemma (lemmaSelector thyLoadOptions))
             <=< (return . removeTranslationItems)
             <=< Sapic.typeTheory
             <=< Sapic.warnings
-          ModuleProVerif              -> Export.prettyProVerifTheory (lemmaSelector as) <=< Sapic.typeTheoryEnv <=< Sapic.warnings
-          ModuleProVerifEquivalence   -> Export.prettyProVerifEquivTheory <=< Sapic.typeTheoryEnv <=< Sapic.warnings
-          ModuleDeepSec               -> Export.prettyDeepSecTheory <=< Sapic.typeTheory <=< Sapic.warnings
-
-        out :: (a -> Pretty.Doc) -> (a -> IO Pretty.Doc) -> IO a -> IO Pretty.Doc
-        out summaryDoc fullDoc load
-          | dryRun    = do
-              thy <- load
-              doc <- fullDoc thy
-              putStrLn $ renderDoc doc
-              return $ ppAnalyzed Pretty.$--$ Pretty.nest 2 (summaryDoc thy)
-          | otherwise = do
-              putStrLn $ ""
-              putStrLn $ "analyzing: " ++ inFile
-              putStrLn $ ""
-              let outFile = mkOutPath inFile
-              (thySummary, t) <- timed $ do
-                  thy <- load
-                  doc <- fullDoc thy
-                  writeFileWithDirs outFile $ renderDoc doc
-                  -- ensure that the summary is in normal form
-                  evaluate $ force $ summaryDoc thy
-              let summary = Pretty.vcat
-                    [ ppAnalyzed
-                    , Pretty.text $ ""
-                    , Pretty.text $ "  output:          " ++ outFile
-                    , Pretty.text $ "  processing time: " ++ show t
-                    , Pretty.text $ ""
-                    , Pretty.nest 2 thySummary
-                    ]
-              putStrLn $ replicate 78 '-'
-              putStrLn $ renderDoc summary
-              putStrLn $ ""
-              putStrLn $ replicate 78 '-'
-              return summary
-
-    {- TO BE REACTIVATED once infrastructure from interactive mode can be used
-
-    -- static html generation
-    -------------------------
-
-    generateHtml :: FilePath      -- ^ Input file
-                 -> ClosedTheory  -- ^ Theory to pretty print
-                 -> IO ()
-    generateHtml inFile thy = do
-      cmdLine  <- getCommandLine
-      time     <- getCurrentTime
-      cpu      <- getCpuModel
-      template <- getHtmlTemplate
-      theoryToHtml $ GenerationInput {
-          giHeader      = "Generated by " ++ htmlVersionStr
-        , giTime        = time
-        , giSystem      = cpu
-        , giInputFile   = inFile
-        , giTemplate    = template
-        , giOutDir      = mkOutPath inFile
-        , giTheory      = thy
-        , giCmdLine     = cmdLine
-        , giCompress    = not $ argExists "noCompress" as
-        }
-
-    -}
+          Just ModuleProVerif              -> Export.prettyProVerifTheory (lemmaSelector thyLoadOptions) <=< Sapic.typeTheoryEnv <=< Sapic.warnings
+          Just ModuleProVerifEquivalence   -> Export.prettyProVerifEquivTheory <=< Sapic.typeTheoryEnv <=< Sapic.warnings
+          Just ModuleDeepSec               -> Export.prettyDeepSecTheory <=< Sapic.typeTheory <=< Sapic.warnings

lib/accountability/src/Accountability.hs
@@ -7,10 +7,11 @@
 -- Translation from OpenTheories to OpenTheories with accountability lemmas
 
 module Accountability (
-       translate
+       module Accountability.Generation
+     , translate
 ) where
 import Control.Monad.Catch (MonadThrow (throwM), MonadCatch, Exception)
-import Theory (OpenTheory, CaseIdentifier, theoryAccLemmas, aCaseIdentifiers, cName, aName, aCaseTests, AccLemma)
+import Theory (OpenTheory, CaseIdentifier, theoryAccLemmas, aCaseIdentifiers, cName, aName, aCaseTests, AccLemma, caseTestToPredicate, theoryCaseTests)
 import Data.Data (Typeable)
 import Control.Applicative (Alternative)
 import qualified Data.Label as L
@@ -18,7 +19,8 @@ import Data.List (intercalate, (\\))
 import Data.Maybe (mapMaybe)
 import Control.Monad (unless, guard, foldM)
 import Accountability.Generation (generateAccountabilityLemmas)
-import Theory.Text.Parser
+import Theory.Text.Parser (liftedAddLemma)
+import Theory.Text.Parser.Signature (liftedAddPredicate)
 
 
 ------------------------------------------------------------------------------
@@ -42,7 +44,10 @@ translate thy = do
     let undef = mapMaybe undefinedCaseTests (theoryAccLemmas thy)
     unless (null undef) (throwM (CaseTestsUndefined undef :: AccException))
     accLemmas <- mapM generateAccountabilityLemmas (theoryAccLemmas thy)
-    foldM liftedAddLemma thy (concat accLemmas)
+    thy' <- foldM liftedAddLemma thy (concat accLemmas)
+    let casePredicates = mapMaybe caseTestToPredicate (theoryCaseTests thy)
+    foldM liftedAddPredicate thy' casePredicates
+
 
 -- | Checks if the case tests requiered by an accountability lemma are present
 undefinedCaseTests :: Alternative f => AccLemma -> f (String, [CaseIdentifier])

lib/accountability/src/Accountability/Generation.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
--- Copyright   : (c) 2019-2021 Robert Künnemann, Kevin Morio & Yavor Ivanov
+-- Copyright   : (c) 2019-2022 Robert Künnemann, Kevin Morio & Yavor Ivanov
 -- License     : GPL v3 (see LICENSE)
 --
 -- Maintainer  : Robert Künnemann <robert@kunnemann.de>
@@ -8,7 +8,7 @@
 -- Compute translation-specific restrictions
 module Accountability.Generation (
         generateAccountabilityLemmas
-      , checkPreTransWellformedness
+      , checkWellformedness
 ) where
 
 import           Control.Monad.Catch         (MonadThrow)
@@ -332,5 +332,5 @@ accRPReport thy
                           $-$ text "and the parties corrupted in t’’ are the same as the parties"
                           $-$ text "corrupted in t’ renamed from rng(ρ’) to rng(ρ)."
 
-checkPreTransWellformedness :: OpenTheory -> WfErrorReport
-checkPreTransWellformedness thy = if null $ theoryAccLemmas thy then [] else accRPReport thy
+checkWellformedness :: OpenTheory -> WfErrorReport
+checkWellformedness thy = if null $ theoryAccLemmas thy then [] else accRPReport thy

lib/sapic/src/Sapic/Warnings.hs
@@ -29,5 +29,5 @@ throwWarningsProcess p = traverse_ throwM capture_warnings >> return p -- search
 warnings :: (Monad m, MonadThrow m) => Theory sig c r p TranslationElement -> m (Theory sig c r p TranslationElement)
 warnings = mapMProcesses throwWarningsProcess
 
-checkWellformednessSapic :: OpenTheory  -> WfErrorReport
-checkWellformednessSapic = concatMap (toWfErrorReport . warnProcess) . theoryProcesses
+checkWellformedness :: OpenTheory  -> WfErrorReport
+checkWellformedness = concatMap (toWfErrorReport . warnProcess) . theoryProcesses

lib/theory/src/Items/AccLemmaItem.hs
@@ -11,12 +11,12 @@ module Items.AccLemmaItem (
 import GHC.Generics (Generic)
 import Control.DeepSeq (NFData)
 import Data.Binary (Binary)
+import Data.List
 import Data.Label as L
 import Text.PrettyPrint.Highlight
 import Theory.Text.Pretty
 import Theory.Model
 import Lemma
-
 import Items.CaseTestItem
 
 ------------------------------------------------------------------------------
@@ -48,6 +48,10 @@ prettyAccLemma :: HighlightDocument d => AccLemma -> d
 prettyAccLemma alem =
     kwLemma <-> prettyAccLemmaName alem <> colon $-$
     (nest 2 $
+      text (intercalate ", " (L.get aCaseIdentifiers alem)) <-> account $-$
       sep [  doubleQuotes $ prettySyntacticLNFormula $ L.get aFormula alem
           ]
     )
+    where
+        account | length (L.get aCaseIdentifiers alem) == 1 = text "accounts for"
+                | otherwise                                 = text "accounts for"

lib/theory/src/Items/CaseTestItem.hs
@@ -14,6 +14,7 @@ import Data.Binary (Binary)
 import Data.Label as L
 import Theory.Model
 import Theory.Syntactic.Predicate
+import Text.PrettyPrint.Highlight (HighlightDocument, Document (nest, (<->), ($-$), text, sep), colon, doubleQuotes)
 
 ------------------------------------------------------------------------------
 -- Case Tests
@@ -35,3 +36,11 @@ caseTestToPredicate caseTest = fmap (Predicate fact) formula
     fact = protoFact Linear name (frees formula)
     name = L.get cName caseTest
     formula = toLNFormula (L.get cFormula caseTest)
+
+prettyCaseTest :: HighlightDocument d => CaseTest -> d
+prettyCaseTest caseTest =
+    text "test" <-> text (L.get cName caseTest) <> colon $-$
+    (nest 2 $
+      sep [  doubleQuotes $ prettySyntacticLNFormula $ L.get cFormula caseTest
+          ]
+    )

lib/theory/src/Items/LemmaItem.hs
@@ -3,11 +3,15 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 module Items.LemmaItem (
     module Items.LemmaItem
 ) where
 
+import GHC.Records
 import GHC.Generics (Generic)
 import Control.DeepSeq (NFData)
 import Data.Binary (Binary)
@@ -73,8 +77,19 @@ data DiffLemma p = DiffLemma
        deriving( Eq, Ord, Show, Generic, NFData, Binary )
 $(mkLabels [''DiffLemma])
 
+type HasLemmaName l = HasField "lName" l String
 
+instance HasField "lName" (ProtoLemma f p) String where
+  getField = _lName
+instance HasField "lName" (DiffLemma p) String where
+  getField = _lDiffName
 
+type HasLemmaAttributes l = HasField "lAttributes" l [LemmaAttribute]
+
+instance HasField "lAttributes" (ProtoLemma f p) [LemmaAttribute] where
+  getField = _lAttributes
+instance HasField "lAttributes" (DiffLemma p) [LemmaAttribute] where
+  getField = _lDiffAttributes
 
 
 -- Instances

lib/theory/src/Prover.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Rank2Types #-}
+
 module Prover (
     module Prover
 ) where
@@ -66,10 +68,10 @@ closeDiffTheoryWithMaude :: SignatureWithMaude -> OpenDiffTheory -> Bool -> Clos
 closeDiffTheoryWithMaude sig thy0 autoSources =
   if autoSources && (containsPartialDeconstructions (cacheLeft items) || containsPartialDeconstructions (cacheRight items))
     then
-      proveDiffTheory (const True) (const True) checkProof checkDiffProof
+      proveDiffTheory (const True) checkProof checkDiffProof
         (DiffTheory (L.get diffThyName thy0) h sig (cacheLeft items') (cacheRight items') (diffCacheLeft items') (diffCacheRight items') items')
     else
-      proveDiffTheory (const True) (const True) checkProof checkDiffProof
+      proveDiffTheory (const True) checkProof checkDiffProof
         (DiffTheory (L.get diffThyName thy0) h sig (cacheLeft items) (cacheRight items) (diffCacheLeft items) (diffCacheRight items) items)
   where
     h              = L.get diffThyHeuristic thy0
@@ -264,13 +266,12 @@ proveTheory selector prover thy =
 
 -- | Prove both the assertion soundness as well as all lemmas of the theory. If
 -- the prover fails on a lemma, then its proof remains unchanged.
-proveDiffTheory :: (Lemma IncrementalProof -> Bool)       -- ^ Lemma selector.
-            -> (DiffLemma IncrementalDiffProof -> Bool)   -- ^ DiffLemma selector.
-            -> Prover
-            -> DiffProver
-            -> ClosedDiffTheory
-            -> ClosedDiffTheory
-proveDiffTheory selector diffselector prover diffprover thy =
+proveDiffTheory :: (forall l. (HasLemmaName l, HasLemmaAttributes l) => l -> Bool)       -- ^ Lemma selector.
+                   -> Prover
+                   -> DiffProver
+                   -> ClosedDiffTheory
+                   -> ClosedDiffTheory
+proveDiffTheory selector prover diffprover thy =
     modify diffThyItems ((`MS.evalState` []) . mapM prove) thy
   where
     prove item = case item of
@@ -291,7 +292,7 @@ proveDiffTheory selector diffselector prover diffprover thy =
         add prf = fromMaybe prf $ runProver prover ctxt 0 sys prf
 
     proveDiffLemma lem preItems
-      | diffselector lem = modify lDiffProof add lem
+      | selector lem = modify lDiffProof add lem
       | otherwise        = lem
       where
         ctxt    = getDiffProofContext lem thy

commit a8107748bd57d80b41f2f8bf8a756f2cbb4c5049

Fix reporting of processing time (#517)
Fixes a regression introduced in #484 where the processing time was incorrectly measured due to the missing full evaluation of the IO action. Additionally, the precission of the reported time is now set to 2.

lib/utils/src/System/Timing.hs
@@ -1,27 +1,34 @@
 -- |
--- Copyright   : (c) 2011 Simon Meier
+-- Copyright   : (c) 2011 Simon Meier, 2022 Kevin Morio
 -- License     : GPL v3 (see LICENSE)
 --
 -- Maintainer  : Simon Meier <iridcode@gmail.com>
 -- Portability : GHC only
 --
--- A simple module for timing IO action.
+-- A simple module for timing IO actions and evaluation of values.
 module System.Timing (
-    timed
-  , timed_
+    timedIO
+  , timed
 ) where
 
-import           Control.Monad
-import           Data.Time.Clock
+import Data.Time.Clock ( diffUTCTime, getCurrentTime, NominalDiffTime )
+import GHC.IO (unsafePerformIO)
+import Control.DeepSeq ( NFData, deepseq )
 
--- | Execute an IO action and return its result plus the time it took to execute it.
-timed :: IO a -> IO (a, NominalDiffTime)
-timed io = do
-  t0 <- getCurrentTime
-  x <- io
-  t1 <- getCurrentTime
-  return (x, diffUTCTime t1 t0)
+-- | Fully evaluate an IO action and measure its execution time.
+timedIO :: NFData a => IO a -> IO (a, NominalDiffTime)
+timedIO mx = do
+    t0 <- getCurrentTime
+    val <- mx
+    t1 <- val `deepseq` getCurrentTime
+    let measure = t1 `diffUTCTime` t0
+    return (val, measure)
 
--- | Execute an IO action and return the time it took to execute it.
-timed_ :: IO a -> IO NominalDiffTime
-timed_ = (snd `liftM`) . timed
+-- | Fully evaluate a value and measure its execution time.
+timed :: NFData a => a -> (a, NominalDiffTime)
+timed x =
+    let measure = unsafePerformIO $ do
+                    t0 <- getCurrentTime
+                    t1 <- x `deepseq` getCurrentTime
+                    return (t1 `diffUTCTime` t0)
+    in (x, measure)

src/Main/Mode/Batch.hs
@@ -16,7 +16,7 @@ import           Data.List
 import           Data.Bitraversable              (bisequence)
 import           System.Console.CmdArgs.Explicit as CmdArgs
 import           System.FilePath
-import           System.Timing                   (timed)
+import           System.Timing                   (timedIO)
 import           Extension.Data.Label
 
 import qualified Text.PrettyPrint.Class          as Pretty
@@ -35,6 +35,7 @@ import           Theory.Module
 import           Control.Monad.Except (MonadIO(liftIO), runExceptT)
 import           System.Exit (die)
 import Theory.Tools.Wellformedness (prettyWfErrorReport)
+import           Text.Printf                     (printf)
 
 
 -- | Batch processing mode.
@@ -89,9 +90,8 @@ run thisMode as
       putStrLn ""
   | otherwise = do
       versionData <- ensureMaudeAndGetVersion as
-      res <- mapM (timed . processThy versionData) inFiles
-      let (thys, times) = unzip res
-      let (docs, reps) = unzip thys
+      resTimed <- mapM (timedIO . processThy versionData) inFiles
+      let (docs, reps, times) = unzip3 $ fmap (\((d, r), t) -> (d, r, t)) resTimed
 
       if writeOutput then do
         let maybeOutFiles = sequence $ mkOutPath <$> inFiles
@@ -125,7 +125,7 @@ run thisMode as
                   , Pretty.text $ ""
                   , Pretty.nest 2 $ Pretty.vcat [
                       maybe Pretty.emptyDoc (\o -> Pretty.text $ "output:          " ++ o) outFile
-                    , Pretty.text $ "processing time: " ++ show time
+                    , Pretty.text $ printf "processing time: %.2fs" (realToFrac time :: Double)
                     , Pretty.text $ ""
                     , summary ] ]
 