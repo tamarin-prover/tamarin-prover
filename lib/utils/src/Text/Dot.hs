9f46c0acfebe7279b57320606d7db46abf66e02f
Adrian Dapprich <adrian@dappri.ch>
2024-05-23 18:01:53 +0200

    Refactor implementation of dot generation.
    I replace the custom dot monad with a standard state monad which threads
    through the counter for the current node id.

--- lib/utils/src/Text/Dot.hs
+++ lib/utils/src/Text/Dot.hs
@@ -9,6 +9,7 @@
 --
 -- This module provides a simple interface for building .dot graph files, for input into the dot and graphviz tools.
 -- It includes a monadic interface for building graphs.
+{-# LANGUAGE TemplateHaskell #-}
 
 module Text.Dot
         (
@@ -53,6 +54,8 @@ module Text.Dot
 import Data.Char           (isSpace)
 import Data.List           (intersperse)
 import Control.Monad       (liftM, ap)
+import Control.Monad.State (State, evalState, runState)
+import           Extension.Data.Label
 -- import Control.Applicative (Applicative(..))
 
 data NodeId = NodeId String
@@ -68,28 +71,38 @@ data GraphElement = GraphAttribute String String
                   | GraphNode NodeId        [(String,String)]
                   | GraphEdge NodeId NodeId [(String,String)]
                   | Scope           [GraphElement]
-                  | SubGraph NodeId [GraphElement]
+                  | SubGraph (Maybe NodeId) [GraphElement]
+  deriving (Show)
 
-data Dot a = Dot { unDot :: Int -> ([GraphElement],Int,a) }
+data DotGenState = DotGenState {
+  _dgsId :: Int,
+  _dgsElements :: [GraphElement]
+}
 
-instance Functor Dot where
-  fmap f m = Dot $ \ uq -> case unDot m uq of (a,b,x) -> (a,b,f x)
+$(mkLabels [''DotGenState])
 
-instance Applicative Dot where
-  pure  = return
-  (<*>) = ap
+type Dot = State DotGenState
 
-instance Monad Dot where
-  return a = Dot $ \ uq -> ([],uq,a)
-  m >>= k  = Dot $ \ uq -> case unDot m uq of
-                           (g1,uq',r) -> case unDot (k r) uq' of
-                                           (g2,uq2,r2) -> (g1 ++ g2,uq2,r2)
+runDot :: DotGenState -> Dot a -> (a, DotGenState)
+runDot state dot = runState dot state
+
+nextId :: Dot Int
+nextId = do
+  uq <- getM dgsId
+  () <- setM dgsId (succ uq)
+  return uq
+  
+addElements :: [GraphElement] -> Dot ()
+addElements elems = do
+  modM dgsElements (++elems)
 
 -- | 'rawNode' takes a list of attributes, generates a new node, and gives a 'NodeId'.
 rawNode      :: [(String,String)] -> Dot NodeId
-rawNode attrs = Dot $ \ uq ->
+rawNode attrs = do 
+    uq <- nextId
     let nid = NodeId $ "n" ++ show uq
-    in ( [ GraphNode nid attrs ],succ uq,nid)
+    addElements [ GraphNode nid attrs ]
+    return nid
 
 -- | 'node' takes a list of attributes, generates a new node, and gives a
 -- 'NodeId'. Multi-line labels are fixed such that they use non-breaking
@@ -101,30 +114,32 @@ node = rawNode . map fixLabel
     fixLabel attr          = attr
 
 
--- | 'userNodeId' allows a user to use their own (Int-based) node id's, without needing to remap them.
-userNodeId :: Int -> NodeId
-userNodeId i = UserNodeId i
-
 -- | 'userNode' takes a NodeId, and adds some attributes to that node.
 userNode :: NodeId -> [(String,String)] -> Dot ()
-userNode nId attrs = Dot $ \ uq -> ( [GraphNode nId attrs ],uq,())
+userNode nId attrs = addElements [GraphNode nId attrs ]
 
 -- | 'edge' generates an edge between two 'NodeId's, with attributes.
 edge      :: NodeId -> NodeId -> [(String,String)] -> Dot ()
-edge  from to attrs = Dot (\ uq -> ( [ GraphEdge from to attrs ],uq,()))
+edge  from to attrs = addElements [ GraphEdge from to attrs ]
+
 
 -- | 'scope' groups a subgraph together; in dot these are the subgraphs inside "{" and "}".
 scope     :: Dot a -> Dot a
-scope (Dot fn) = Dot (\ uq -> case fn uq of
-                              ( elems,uq',a) -> ([Scope elems],uq',a))
+scope dot = do 
+  uq <- getM dgsId
+  let subState = DotGenState { _dgsId = uq, _dgsElements = [] }
+  let (a, finalState) = runDot subState dot
+  setM dgsId (get dgsId finalState)
+  addElements [SubGraph Nothing (get dgsElements finalState)]
+  return a
 
 -- | 'share' is when a set of nodes share specific attributes. Usually used for layout tweaking.
 share :: [(String,String)] -> [NodeId] -> Dot ()
-share attrs nodeids = Dot $ \ uq ->
-      ( [ Scope ( [ GraphAttribute name val | (name,val) <- attrs]
+share attrs nodeids =
+  let elems = [ Scope ( [ GraphAttribute name val | (name,val) <- attrs]
                ++ [ GraphNode nodeid [] | nodeid <- nodeids ]
-               )
-        ], uq, ())
+               ) ] 
+  in addElements elems
 
 -- | 'same' provides a combinator for a common pattern; a set of 'NodeId's with the same rank.
 same :: [NodeId] -> Dot ()
@@ -133,39 +148,124 @@ same = share [("rank","same")]
 
 -- | 'cluster' builds an explicit, internally named subgraph (called cluster).
 cluster :: Dot a -> Dot (NodeId,a)
-cluster (Dot fn) = Dot (\ uq ->
-                let cid = NodeId $ "cluster_" ++ show uq
-                in case fn (succ uq) of
-                    (elems,uq',a) -> ([SubGraph cid elems],uq',(cid,a)))
+cluster dot = do
+  uq <- getM dgsId  
+  let cid = NodeId $ "cluster_" ++ show uq
+  let clusterState = DotGenState { _dgsId = succ uq, _dgsElements = [] }
+  let (a, finalState) = runDot clusterState dot
+  setM dgsId (get dgsId finalState)
+  addElements [SubGraph (Just cid) (get dgsElements finalState)]
+  return (cid, a)
+
 
 -- | 'attribute' gives a attribute to the current scope.
 attribute :: (String,String) -> Dot ()
-attribute (name,val) = Dot (\ uq -> ( [  GraphAttribute name val ],uq,()))
+attribute (name,val) = addElements [  GraphAttribute name val ]
 
 -- | 'nodeAttributes' sets attributes for all the following nodes in the scope.
 nodeAttributes :: [(String,String)] -> Dot ()
-nodeAttributes attrs = Dot (\uq -> ([ GraphNode (NodeId "node") attrs],uq,()))
+nodeAttributes attrs = addElements [ GraphNode (NodeId "node") attrs]
 
 -- | 'edgeAttributes' sets attributes for all the following edges in the scope.
 edgeAttributes :: [(String,String)] -> Dot ()
-edgeAttributes attrs = Dot (\uq -> ([ GraphNode (NodeId "edge") attrs],uq,()))
+edgeAttributes attrs = addElements [ GraphNode (NodeId "edge") attrs]
 
 -- | 'graphAttributes' sets attributes for current graph.
 graphAttributes :: [(String,String)] -> Dot ()
-graphAttributes attrs = Dot (\uq -> ([ GraphNode (NodeId "graph") attrs],uq,()))
-
+graphAttributes attrs = addElements [ GraphNode (NodeId "graph") attrs]
 
 -- 'showDot' renders a dot graph as a 'String'.
 showDot :: Dot a -> String
-showDot (Dot dm) = case dm 0 of
-                    (elems,_,_) -> "digraph G {\n" ++ unlines (map showGraphElement elems) ++ "\n}\n"
+showDot dot = 
+  let initialState = DotGenState { _dgsId = 0, _dgsElements = [] } 
+      (_, finalState) = runDot initialState dot
+      elems = get dgsElements finalState
+  in 
+    "digraph G {\n" ++ unlines (map showGraphElement elems) ++ "\n}\n"
+
+-- | Render a record in the Dot monad. It exploits the internal counter in
+-- order to generate unique port-ids. However, they must be combined with the
+-- actual node id of the node with the record shape. Thus the returned
+-- association list is parametrized over this missing node id.
+renderRecord :: Record a -> Dot (String, NodeId -> [(a,NodeId)])
+renderRecord = render True
+  where
+  render _ (Field Nothing l) = return (escape l, const [])
+  render _ (Field (Just p) l) = do
+    uq <- nextId
+    let pid = "n" ++ show uq
+    let lbl = "<"++pid++"> "++escape l
+    return (lbl, \nId -> [(p,NodeId (show nId++":"++pid))])
+  render horiz (HCat rs) = do
+    (lbls, ids) <- liftM unzip $ mapM (render True) rs
+    let rawLbl = concat (intersperse "|" lbls)
+        lbl = if horiz then "{{"++rawLbl++"}}" else "{"++rawLbl++"}"
+    return (lbl, \nId -> concatMap (\i -> i nId) ids)
+  render horiz (VCat rs) = do
+    (lbls, ids) <- liftM unzip $ mapM (render False) rs
+    let rawLbl = concat (intersperse "|" lbls)
+        lbl = if horiz then "{"++rawLbl++"}" else "{{"++rawLbl++"}}"
+    return (lbl, \nId -> concatMap (\i -> i nId) ids)
+  -- escape chars used for record label construction
+  escape = concatMap esc
+  esc '|'  = "\\|"
+  esc '{'  = "\\{"
+  esc '}'  = "\\}"
+  esc '<'  = "\\<"
+  esc '>'  = "\\>"
+  esc c    = [c]
+
+
+-- | A generic version of record creation.
+genRecord :: String -> Record a -> [(String,String)] -> Dot (NodeId, [(a,NodeId)])
+genRecord shape rec attrs = do
+  (lbl, ids) <- renderRecord rec
+  i <- rawNode ([("shape",shape),("label",lbl)] ++ attrs)
+  return (i, ids i)
+
+-- | Create a record node with the given attributes. It returns the node-id of
+-- the created node together with an association list mapping the port
+-- idenfiers given in the record to their corresponding node-ids. This list is
+-- ordered according to a left-to-rigth traversal of the record description.
+record :: Record a -> [(String,String)] -> Dot (NodeId, [(a,NodeId)])
+record = genRecord "record"
+
+-- | A variant of "record" ignoring the port identifiers.
+record' :: Record a -> [(String,String)] -> Dot (NodeId, [NodeId])
+record' rec attrs = do (nId, ids) <- record rec attrs
+                       return (nId, map snd ids)
+
+-- | A variant of "record" ignoring the port to node-id association list.
+record_ :: Record a -> [(String,String)] -> Dot NodeId
+record_ rec attrs = liftM fst $ record rec attrs
+
+-- | Like "record", but creates record nodes with rounded corners; i.e. uses
+-- the \"Mrecord\" shape instead of the \"record\" shape.
+mrecord :: Record a -> [(String,String)] -> Dot (NodeId, [(a,NodeId)])
+mrecord = genRecord "Mrecord"
+
+-- | A variant of "mrecord" ignoring the port identifiers.
+mrecord' :: Record a -> [(String,String)] -> Dot (NodeId, [NodeId])
+mrecord' rec attrs = do (nId, ids) <- mrecord rec attrs
+                        return (nId, map snd ids)
+
+-- | A variant of "mrecord" ignoring the port to node-id association list.
+mrecord_ :: Record a -> [(String,String)] -> Dot NodeId
+mrecord_ rec attrs = liftM fst $ mrecord rec attrs
+  
+
+-- | 'userNodeId' allows a user to use their own (Int-based) node id's, without needing to remap them.
+userNodeId :: Int -> NodeId
+userNodeId i = UserNodeId i
+
 
 showGraphElement :: GraphElement -> String
 showGraphElement (GraphAttribute name val) = showAttr (name,val) ++ ";"
 showGraphElement (GraphNode nid attrs)           = show nid ++ showAttrs attrs ++ ";"
 showGraphElement (GraphEdge from to attrs) = show from ++ " -> " ++ show to ++  showAttrs attrs ++ ";"
 showGraphElement (Scope elems) = "{\n" ++ unlines (map showGraphElement elems) ++ "\n}"
-showGraphElement (SubGraph nid elems) = "subgraph " ++ show nid ++ " {\n" ++ unlines (map showGraphElement elems) ++ "\n}"
+showGraphElement (SubGraph Nothing elems) = "{\n" ++ unlines (map showGraphElement elems) ++ "\n}"
+showGraphElement (SubGraph (Just nid) elems) = "subgraph " ++ show nid ++ " {\n" ++ unlines (map showGraphElement elems) ++ "\n}"
 
 showAttrs :: [(String, String)] -> String
 showAttrs [] = ""
@@ -177,8 +277,9 @@ showAttrs xs = "[" ++ showAttrs' xs ++ "]"
         showAttrs' []     = error "showAttrs: the impossible happended"
 
 showAttr :: (String, String) -> String
-showAttr (name, val) =
-      name ++ "=\"" ++ concatMap escape val ++ "\""
+showAttr (name, val) 
+  | name == "raw_label" = "label=" ++ val
+  | otherwise = name ++ "=\"" ++ concatMap escape val ++ "\""
     where
       escape '\n' = "\\l"
       escape '"'  = "\\\""
@@ -238,73 +339,3 @@ vcat = VCat
 -- | Concatenate a list strings interpreted as simple fields vertically.
 vcat' :: [String] -> Record a
 vcat' = vcat . map field
-
--- | Render a record in the Dot monad. It exploits the internal counter in
--- order to generate unique port-ids. However, they must be combined with the
--- actual node id of the node with the record shape. Thus the returned
--- association list is parametrized over this missing node id.
-renderRecord :: Record a -> Dot (String, NodeId -> [(a,NodeId)])
-renderRecord = render True
-  where
-  render _ (Field Nothing l) = return (escape l, const [])
-  render _ (Field (Just p) l) =
-    Dot $ \uq -> let pid = "n" ++ show uq
-                     lbl = "<"++pid++"> "++escape l
-                 in  ([], succ uq, (lbl, \nId -> [(p,NodeId (show nId++":"++pid))]))
-  render horiz (HCat rs) = do
-    (lbls, ids) <- liftM unzip $ mapM (render True) rs
-    let rawLbl = concat (intersperse "|" lbls)
-        lbl = if horiz then "{{"++rawLbl++"}}" else "{"++rawLbl++"}"
-    return (lbl, \nId -> concatMap (\i -> i nId) ids)
-  render horiz (VCat rs) = do
-    (lbls, ids) <- liftM unzip $ mapM (render False) rs
-    let rawLbl = concat (intersperse "|" lbls)
-        lbl = if horiz then "{"++rawLbl++"}" else "{{"++rawLbl++"}}"
-    return (lbl, \nId -> concatMap (\i -> i nId) ids)
-  -- escape chars used for record label construction
-  escape = concatMap esc
-  esc '|'  = "\\|"
-  esc '{'  = "\\{"
-  esc '}'  = "\\}"
-  esc '<'  = "\\<"
-  esc '>'  = "\\>"
-  esc c    = [c]
-
-
--- | A generic version of record creation.
-genRecord :: String -> Record a -> [(String,String)] -> Dot (NodeId, [(a,NodeId)])
-genRecord shape rec attrs = do
-  (lbl, ids) <- renderRecord rec
-  i <- rawNode ([("shape",shape),("label",lbl)] ++ attrs)
-  return (i, ids i)
-
--- | Create a record node with the given attributes. It returns the node-id of
--- the created node together with an association list mapping the port
--- idenfiers given in the record to their corresponding node-ids. This list is
--- ordered according to a left-to-rigth traversal of the record description.
-record :: Record a -> [(String,String)] -> Dot (NodeId, [(a,NodeId)])
-record = genRecord "record"
-
--- | A variant of "record" ignoring the port identifiers.
-record' :: Record a -> [(String,String)] -> Dot (NodeId, [NodeId])
-record' rec attrs = do (nId, ids) <- record rec attrs
-                       return (nId, map snd ids)
-
--- | A variant of "record" ignoring the port to node-id association list.
-record_ :: Record a -> [(String,String)] -> Dot NodeId
-record_ rec attrs = liftM fst $ record rec attrs
-
--- | Like "record", but creates record nodes with rounded corners; i.e. uses
--- the \"Mrecord\" shape instead of the \"record\" shape.
-mrecord :: Record a -> [(String,String)] -> Dot (NodeId, [(a,NodeId)])
-mrecord = genRecord "Mrecord"
-
--- | A variant of "mrecord" ignoring the port identifiers.
-mrecord' :: Record a -> [(String,String)] -> Dot (NodeId, [NodeId])
-mrecord' rec attrs = do (nId, ids) <- mrecord rec attrs
-                        return (nId, map snd ids)
-
--- | A variant of "mrecord" ignoring the port to node-id association list.
-mrecord_ :: Record a -> [(String,String)] -> Dot NodeId
-mrecord_ rec attrs = liftM fst $ mrecord rec attrs
-
