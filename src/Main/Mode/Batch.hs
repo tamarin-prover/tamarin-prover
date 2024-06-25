tamarin-prover/tamarin-prover

commit 86014ad1844247b4faac36bc15f83befde30dcd7

Run wellformedness checks in parse-only mode and on export (#595)
* Run wellformedness checks in parse-only mode and on export

Wellformedness checks are now also executed in parse-only mode and when
exporting to other formats. Warnings and the version string are included
as comments in tho output.

* Revise theory loading and translation based on output module

* Update regression case studies

src/Main/Mode/Batch.hs
@@ -12,7 +12,7 @@ module Main.Mode.Batch (
 
 import           Control.Basics
 import           Data.List
-import           Data.Bitraversable              (bisequence)
+import           Data.Bitraversable              (bitraverse, bisequence)
 import           System.Console.CmdArgs.Explicit as CmdArgs
 import           System.FilePath
 import           System.Timing                   (timedIO)
@@ -21,9 +21,6 @@ import           Extension.Data.Label
 import qualified Text.PrettyPrint.Class          as Pretty
 
 import           Theory hiding (closeTheory)
-
-import qualified Sapic
-import qualified Export
 
 import           Main.Console
 import           Main.Environment
@@ -35,7 +32,6 @@ import           Control.Monad.Except (MonadIO(liftIO), runExceptT)
 import           System.Exit (die)
 import Theory.Tools.Wellformedness (prettyWfErrorReport)
 import           Text.Printf                     (printf)
-
 
 -- | Batch processing mode.
 batchMode :: TamarinMode
@@ -80,13 +76,17 @@ batchMode = tamarinMode
 run :: TamarinMode -> Arguments -> IO ()
 run thisMode as
   | null inFiles = helpAndExit thisMode (Just "no input files given")
-  | argExists "parseOnly" as || argExists "outModule" as = do
-
+  | argExists "parseOnly" as = do
       res <- mapM (processThy "") inFiles
-      let (thys, _) = unzip res
+      let (docs, _) = unzip res
 
-      mapM_ (putStrLn . renderDoc) thys
-      putStrLn ""
+      mapM_ (putStrLn . renderDoc) docs
+  | argExists "outModule" as = do
+      versionData <- ensureMaudeAndGetVersion as
+      res <- mapM (processThy versionData) inFiles
+      let (docs, _) = unzip res
+
+      mapM_ (putStrLn . renderDoc) docs
   | otherwise = do
       versionData <- ensureMaudeAndGetVersion as
       resTimed <- mapM (timedIO . processThy versionData) inFiles
@@ -97,13 +97,13 @@ run thisMode as
         outFiles <- case maybeOutFiles of
           Just f -> return f
           Nothing -> die "Please specify a valid output file/directory"
-        let repsWithInfo = ppRep <$> zip4 inFiles (Just <$> outFiles) times reps
+        let repsWithInfo = ppRep <$> zip4 inFiles (Just <$> outFiles) (Just <$> times) reps
         let summary = Pretty.vcat $ intersperse (Pretty.text "") repsWithInfo
 
         mapM_ (\(o, d) -> writeFileWithDirs o (renderDoc d)) (zip outFiles docs)
         putStrLn $ renderDoc $ ppSummary summary
       else do
-        let repsWithInfo = ppRep <$> zip4 inFiles (repeat Nothing) times reps
+        let repsWithInfo = ppRep <$> zip4 inFiles (repeat Nothing) (Just <$> times) reps
         let summary = Pretty.vcat $ intersperse (Pretty.text "") repsWithInfo
 
         mapM_ (putStrLn . renderDoc) docs
@@ -124,7 +124,7 @@ run thisMode as
                   , Pretty.text $ ""
                   , Pretty.nest 2 $ Pretty.vcat [
                       maybe Pretty.emptyDoc (\o -> Pretty.text $ "output:          " ++ o) outFile
-                    , Pretty.text $ printf "processing time: %.2fs" (realToFrac time :: Double)
+                    , maybe Pretty.emptyDoc (\t -> Pretty.text $ printf "processing time: %.2fs" (realToFrac t :: Double)) time
                     , Pretty.text $ ""
                     , summary ] ]
 
@@ -164,42 +164,45 @@ run thisMode as
       srcThy <- liftIO $ readFile inFile
       thy    <- loadTheory thyLoadOptions srcThy inFile
 
-      if isParseOnlyMode then do
-        either (\t -> bisequence (liftIO $ choosePretty t, return Pretty.emptyDoc))
-               (\d -> return (prettyOpenDiffTheory d, Pretty.emptyDoc)) thy
-      else do
-        let sig = either (get thySignature) (get diffThySignature) thy
-        sig'   <- liftIO $ toSignatureWithMaude (get oMaudePath thyLoadOptions) sig
+      let sig = either (get thySignature) (get diffThySignature) thy
+      sig'   <- liftIO $ toSignatureWithMaude (get oMaudePath thyLoadOptions) sig
 
+      -- | Pretty print the theory as is without performing any checks.
+      if isParseOnlyMode then do
+        either (\t -> return (prettyOpenTheory t, Pretty.emptyDoc))
+               (\d -> return (prettyOpenDiffTheory d, Pretty.emptyDoc)) thy
+
+      -- | Translate and check thoery based on specified output module.
+      else if isTranslateOnlyMode then do
+        (report, thy') <- translateAndCheckTheory versionData thyLoadOptions sig' thy
+
+        thy'' <- bitraverse (return . modify thyItems (++ (TextItem <$> formalComments thy')))
+                            (return . modify diffThyItems (++ (DiffTextItem <$> formalComments thy'))) thy'
+
+        either (\t -> bisequence (liftIO $ prettyOpenTheoryByModule thyLoadOptions t, return $ ppWf report))
+               (\d -> return (prettyOpenDiffTheory d, ppWf report)) thy''
+
+      -- | Close and potentially prove theory.
+      else do
         (report, thy') <- closeTheory versionData thyLoadOptions sig' thy
         either (\t -> return (prettyClosedTheory t,     ppWf report Pretty.$--$ prettyClosedSummary t))
                (\d -> return (prettyClosedDiffTheory d, ppWf report Pretty.$--$ prettyClosedDiffSummary d)) thy'
-
       where
+        formalComments = either (filter (/=("","")) . theoryFormalComments) (filter (/=("", "")) . diffTheoryFormalComments)
+
         isParseOnlyMode = get oParseOnlyMode thyLoadOptions
+        isTranslateOnlyMode = get oOutputModule thyLoadOptions `elem` 
+                                [ModuleSpthy, ModuleSpthyTyped, ModuleProVerif, ModuleProVerifEquivalence, ModuleDeepSec]
 
         handleError e@(ParserError _) = die $ show e
         handleError (WarningError report) = do
-          putStrLn $ renderDoc $ Pretty.vcat [ Pretty.text ""
-                                             , Pretty.text "WARNING: the following wellformedness checks failed!"
-                                             , Pretty.text ""
-                                             , prettyWfErrorReport report
-                                             , Pretty.text "" ]
+          putStrLn $ renderDoc $ Pretty.vcat $ [ Pretty.text ""
+                                               , Pretty.text "WARNING: the following wellformedness checks failed!" ]
+                                            ++ [ Pretty.text "" | not $ null report ]
+                                            ++ [ prettyWfErrorReport report
+                                               , Pretty.text "" ]
           die "quit-on-warning mode selected - aborting on wellformedness errors."
 
         ppWf []  = Pretty.emptyDoc
         ppWf rep = Pretty.vcat $ Pretty.text ("WARNING: " ++ show (length rep) ++ " wellformedness check failed!")
                              : [ Pretty.text   "         The analysis results might be wrong!" | get oProveMode thyLoadOptions ]
-
-        choosePretty = case get oOutputModule thyLoadOptions of
-          Nothing               -> return . prettyOpenTheory  <=< Sapic.warnings -- output as is, including SAPIC elements
-          Just ModuleSpthy      -> return . prettyOpenTheory  <=< Sapic.warnings -- output as is, including SAPIC elements
-          Just ModuleSpthyTyped -> return . prettyOpenTheory <=< Sapic.typeTheory <=< Sapic.warnings  -- additionally type
-          Just ModuleMsr        -> return . prettyOpenTranslatedTheory
-            <=< (return . filterLemma (lemmaSelector thyLoadOptions))
-            <=< (return . removeTranslationItems)
-            <=< Sapic.typeTheory
-            <=< Sapic.warnings
-          Just ModuleProVerif              -> Export.prettyProVerifTheory (lemmaSelector thyLoadOptions) <=< Sapic.typeTheoryEnv <=< Sapic.warnings
-          Just ModuleProVerifEquivalence   -> Export.prettyProVerifEquivTheory <=< Sapic.typeTheoryEnv <=< Sapic.warnings
-          Just ModuleDeepSec               -> Export.prettyDeepSecTheory <=< Sapic.typeTheory <=< Sapic.warnings

commit d7e0fb66d62598e9f751fba25c2da4ef29930654

Fix unintended filtering of lemmas in default output module
This fixes a regression introduced in #595 where the default output
module was changed causing all lemmas not selected via `--prove` or
`--lemma` (inlcuding source lemmas) to be removed from the theory.

src/Main/Mode/Batch.hs
@@ -15,6 +15,7 @@ import Control.Monad.Except (runExceptT)
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Data.Bifunctor (bimap)
 import Data.List
+import Data.Maybe (isJust)
 import System.Console.CmdArgs.Explicit as CmdArgs
 import System.Exit (die)
 import System.FilePath
@@ -208,14 +209,7 @@ run thisMode as
         formalComments =
           filter (/= ("", "")) . either theoryFormalComments diffTheoryFormalComments
 
-        isTranslateOnlyMode =
-          thyLoadOptions._oOutputModule `elem`
-            [ ModuleSpthy
-            , ModuleSpthyTyped
-            , ModuleProVerif
-            , ModuleProVerifEquivalence
-            , ModuleDeepSec
-            ]
+        isTranslateOnlyMode = isJust $ L.get oOutputModule thyLoadOptions
 
         handleError e@(ParserError _) = die $ show e
         handleError (WarningError report) = do

src/Main/TheoryLoader.hs
@@ -181,7 +181,7 @@ data TheoryLoadOptions = TheoryLoadOptions {
   , _oQuitOnWarning     :: Bool
   , _oAutoSources       :: Bool
   , _oVerboseMode       :: Bool
-  , _oOutputModule      :: ModuleType -- Note: This flag is only used for batch mode.
+  , _oOutputModule      :: Maybe ModuleType -- Note: This flag is only used for batch mode.
   , _oMaudePath         :: FilePath -- FIXME: Other functions defined in Environment.hs
   , _oParseOnlyMode     :: Bool
   , _oOpenChain         :: Integer
@@ -203,7 +203,7 @@ defaultTheoryLoadOptions = TheoryLoadOptions {
   , _oQuitOnWarning     = False
   , _oAutoSources       = False
   , _oVerboseMode       = False
-  , _oOutputModule      = ModuleMsr
+  , _oOutputModule      = Nothing
   , _oMaudePath         = "maude"
   , _oParseOnlyMode     = False
   , _oOpenChain         = 10
@@ -276,7 +276,7 @@ mkTheoryLoadOptions as = TheoryLoadOptions
 
     outputModule = case findArg "outModule" as of
       Just str -> case find ((str ==) . show) [minBound..] of
-        Just m -> return m
+        Just m -> return $ Just m
         _       -> throwError $ ArgumentError "output mode not supported."
       Nothing   -> return $ L.get oOutputModule defaultTheoryLoadOptions
 
@@ -306,7 +306,7 @@ stopOnTrace as = case map toLower <$> findArg "stop-on-trace" as of
 lemmaSelectorByModule :: HasLemmaAttributes l => TheoryLoadOptions -> l -> Bool
 lemmaSelectorByModule thyOpt lem = case lemmaModules of
     [] -> True -- default to true if no modules (or only empty ones) are set
-    _  -> (L.get oOutputModule thyOpt) `elem` lemmaModules
+    _  -> maybe True (`elem` lemmaModules) (L.get oOutputModule thyOpt)
     where
         lemmaModules = concat [ m | LemmaModule m <- getField @"lAttributes" lem]
 
@@ -362,12 +362,14 @@ loadTheory thyOpts input inFile = do
 -- | Process an open theory based on the specified output module.
 processOpenTheory :: MonadCatch m => TheoryLoadOptions -> OpenTheory -> m OpenTheory
 processOpenTheory thyOpts = case modType of
-  ModuleSpthy               -> return
-  ModuleSpthyTyped          -> Sapic.typeTheory
-  ModuleMsr                 -> Sapic.typeTheory >=> Sapic.translate >=> Acc.translate >=> (return . filterLemma lemmas)
-  ModuleProVerifEquivalence -> Sapic.typeTheory -- Type theory here to catch errors.
-  ModuleProVerif            -> Sapic.typeTheory -- Type theory here to catch errors.
-  ModuleDeepSec             -> Sapic.typeTheory
+  Nothing                        -> Sapic.typeTheory >=> Sapic.translate >=> Acc.translate
+  Just ModuleSpthy               -> return
+  Just ModuleSpthyTyped          -> Sapic.typeTheory
+  -- If the output module is set to MSR, we only keep the specified lemmas in the theory.
+  Just ModuleMsr                 -> Sapic.typeTheory >=> Sapic.translate >=> Acc.translate >=> (return . filterLemma lemmas)
+  Just ModuleProVerifEquivalence -> Sapic.typeTheory -- Type theory here to catch errors.
+  Just ModuleProVerif            -> Sapic.typeTheory -- Type theory here to catch errors.
+  Just ModuleDeepSec             -> Sapic.typeTheory
   where
     modType = L.get oOutputModule thyOpts
     lemmas = lemmaSelector thyOpts
@@ -539,12 +541,13 @@ translateAndCheckTheory version thyOpts sign srcThy = do
 -- | Pretty print an open theory based on the specified output module.
 prettyOpenTheoryByModule :: TheoryLoadOptions -> OpenTheory -> IO Pretty.Doc
 prettyOpenTheoryByModule thyOpts = case modType of
-  ModuleSpthy               -> return . prettyOpenTheory
-  ModuleSpthyTyped          -> return . prettyOpenTheory
-  ModuleMsr                 -> return . prettyOpenTranslatedTheory . removeTranslationItems
-  ModuleProVerifEquivalence -> Export.prettyProVerifEquivTheory   <=< Sapic.typeTheoryEnv
-  ModuleProVerif            -> Export.prettyProVerifTheory lemmas <=< Sapic.typeTheoryEnv
-  ModuleDeepSec             -> Export.prettyDeepSecTheory
+  Nothing {- Same as ModuleMsr -} -> return . prettyOpenTranslatedTheory . removeTranslationItems
+  Just ModuleSpthy                -> return . prettyOpenTheory
+  Just ModuleSpthyTyped           -> return . prettyOpenTheory
+  Just ModuleMsr                  -> return . prettyOpenTranslatedTheory . removeTranslationItems
+  Just ModuleProVerifEquivalence  -> Export.prettyProVerifEquivTheory   <=< Sapic.typeTheoryEnv
+  Just ModuleProVerif             -> Export.prettyProVerifTheory lemmas <=< Sapic.typeTheoryEnv
+  Just ModuleDeepSec              -> Export.prettyDeepSecTheory
   where
     modType = L.get oOutputModule thyOpts
     lemmas = lemmaSelector thyOpts