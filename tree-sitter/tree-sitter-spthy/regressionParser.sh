a6b4d269adc1af2f0bfb67e2536709d4d2adabd8
Yavor Ivanov <s8yaivan@stud.uni-saarland.de>
2024-08-02 14:14:34 +0200

    regressionParser.sh: added -name *.sapic to the cmd

--- tree-sitter/tree-sitter-spthy/regressionParser.sh
+++ tree-sitter/tree-sitter-spthy/regressionParser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ../../examples -type d \( -name 'deprecated' -o -name 'not-working' \) -prune -o -name '*.spthy' -print0 | xargs -0 tree-sitter parse --quiet --stat
+find ../../examples -type d \( -name 'deprecated' -o -name 'not-working' -o -name 'include' \) -prune -o \( -name '*.spthy' -o -name '*.sapic' \) -print0 | xargs -0 tree-sitter parse --quiet --stat
