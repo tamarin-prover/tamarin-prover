782b0110b0262db6c38b594d47bbffce4b3c01ff
Tom Bloch <61981635+BTom-GH@users.noreply.github.com>
2025-07-15 18:06:56 +0200

    Fix macros pretty printing

--- lib/theory/src/TheoryObject.hs
+++ lib/theory/src/TheoryObject.hs
@@ -811,7 +811,12 @@ prettyVarList = fsep . punctuate comma . map prettyLVar
 
 -- |  Pretty print all macros
 prettyMacros :: (HighlightDocument d) => [Macro] -> d
-prettyMacros m = if m == [] then text empty else vcat (keyword_ "macros:" : map prettyMacro m)
+prettyMacros [] = text empty
+prettyMacros m = keyword_ "macros:" $$ nest 4 
+  (vcat [if i == length m - 1 
+          then prettyMacro macro 
+          else prettyMacro macro <> comma 
+        | (i, macro) <- zip [0..] m])
 
 -- |  Pretty print a macro.
 prettyMacro :: (HighlightDocument d) => Macro -> d
