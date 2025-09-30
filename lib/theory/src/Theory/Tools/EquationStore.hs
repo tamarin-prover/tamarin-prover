fd40598bd35ec2989d1cdacd0ac5797adbbfc7ed
Jannik Dreier <research@jannikdreier.net>
2025-09-30 23:40:34 +0200

    fix renaming check

--- lib/theory/src/Theory/Tools/EquationStore.hs
+++ lib/theory/src/Theory/Tools/EquationStore.hs
@@ -580,52 +580,33 @@ removePermutations hnd eqs splitId v1 v2 =
               t12 = fromMaybe (error $ "Missing image for v2: " ++ show v2 ++ " in subst1: " ++ show subst1) (imageOfVFresh subst1 v2)
               t21 = fromMaybe (error $ "Missing image for v1: " ++ show v1 ++ " in subst2: " ++ show subst2) (imageOfVFresh subst2 v1)
               t22 = fromMaybe (error $ "Missing image for v2: " ++ show v2 ++ " in subst2: " ++ show subst2) (imageOfVFresh subst2 v2)
-          in -- trace (show ("notIsPerm", v1, v2, subst1, subst2, lst1, lst2, t11, t12, t21, t22, not (length lst1 == length lst2
-             --      && all (\(x,t) -> (x == v1) || (x == v2) || (x,t) `elem` lst2) lst1
-             --     && ((t11 == t22 && t12 == t21) || equalUpToRenaming t11 t12 t21 t22 subst1 subst2 || equalUpToRenaming t12 t11 t21 t22 subst1 subst2)
-             --    ))) $
-              not (length lst1 == length lst2 && (
+          in not (length lst1 == length lst2 && (
                   (all (\(x,t) -> (x == v1) || (x == v2) || (x,t) `elem` lst2) lst1 && (t11 == t22 && t12 == t21))
-                  || equalUpToRenaming t11 t12 t21 t22 subst1 subst2
-                  || equalUpToRenaming t11 t12 t22 t21 subst1 subst2
+                  || equalUpToRenaming True subst1 subst2
+                  || equalUpToRenaming False subst1 subst2
                   )
                 )
 
-        equalUpToRenaming :: LNTerm -> LNTerm -> LNTerm -> LNTerm -> LNSubstVFresh -> LNSubstVFresh -> Bool
-        equalUpToRenaming t11 t12 t21 t22 subst1 subst2 = trace (show ("equalUpToRenaming", v1, v2, t11, t12, t21, t22, subst1, varsSubst1, subst2, varsSubst2, eq, unifs, g)) g
+        equalUpToRenaming :: Bool -> LNSubstVFresh -> LNSubstVFresh -> Bool
+        equalUpToRenaming perm subst1 subst2 = trace (show ("equalUpToRenaming", v1, v2, subst1, subst1', subst1'', matchs, subst2, subst2', subst2''', subst2'', matchers, g)) g
           where
-            g = any (\x -> isRenaming (restrictVFresh varsSubst1 x) && isRenaming (restrictVFresh varsSubst2 x) -- && 
-              -- trace (show ("compose", subst1', apply (toLNSubstRaw x) subst1'', substToListVFresh subst2, subst2', apply (toLNSubstRaw x) subst2'', toLNSubstRaw x)) 
-                {-apply (toLNSubstRaw x) subst1'' == apply (toLNSubstRaw x) subst2''-}) {-$ trace (show ("unifs", eq, unifs))-} unifs
+            g = (not . null) matchers
             
-            -- permute subst = substFromListVFresh $ map (\(v,t) -> if v == v1 then (v2,t) else if v == v2 then (v1,t) else (v,t)) (substToListVFresh subst)
+            permute subst = substFromListVFresh $ map (\(v,t) -> if v == v1 then (v2,t) else if v == v2 then (v1,t) else (v,t)) (substToListVFresh subst)
             
-            -- subst1' = substToListVFresh $ if perm then permute subst1 else subst1
-            -- subst2' = substToListVFresh subst2
+            subst1' = substToListVFresh $ if perm then permute subst1 else subst1
+            subst2' = substToListVFresh subst2
             
-            -- subst1''  = apply substFixing subst1'
-            -- subst2''' = apply substFixing subst2'
+            subst1''  = apply substFixing subst1'
+            subst2''' = apply substFixing subst2'
 
-            f11 = apply substFixing t11
-            f12 = apply substFixing t12
-            f21 = apply substFixing t21
-            f22 = apply substFixing t22
-
-            -- subst2''  = renameAvoiding subst2''' ([v1,v2],subst1') 
-            -- toLNSubstRaw x = substFromList $ substToListVFresh x
-
-            (v11, v12)  = (f11, f12)
-            (v21, v22)  = renameAvoiding (f21, f22) ([v1,v2],[f11, f12])
+            subst2'' = zip (map fst subst2''') $ renameAvoidingIgnoring (map snd subst2''') ([v1,v2],subst1'') (map fst subst2''')
             
-            unifs = unifyLNTerm eq `runReader` hnd
-            eq :: [Equal LNTerm]
-            eq = [Equal v11 v22, Equal v12 v21]
-                 -- [Equal (varTerm vs1) (varTerm vs2) | (vs1, vs2) <- zip (map fst subst1'') (map fst subst2'')]
-                 -- ++ [Equal vs1 vs2 | (vs1, vs2) <- zip (map snd subst1'') (map snd subst2'')]
-
-            substFixing = replaceNonMSGVarsWithConstant $ map fst $ varOccurences (t11, t12, t21, t22) -- map fst $ concatMap (varOccurences .snd) (substToListVFresh subst1) ++ concatMap (varOccurences .snd) (substToListVFresh subst2) -- vars
-            varsSubst1 = map fst $ varOccurences (v11, v12)
-            varsSubst2 = map fst $ varOccurences (v21, v22)
+            matchers = solveMatchLNTerm (mconcat matchs) `runReader` hnd
+            matchs :: [Match LNTerm]
+            matchs = zipWith matchWith (map snd subst1'') (map snd subst2'')
+            
+            substFixing = replaceNonMSGVarsWithConstant $ map fst $ concatMap (varOccurences .snd) (substToListVFresh subst1) ++ concatMap (varOccurences .snd) (substToListVFresh subst2) -- map fst $ varOccurences (t11, t12, t21, t22) -- vars
             
             replaceNonMSGVarsWithConstant :: [LVar] -> LNSubst
             replaceNonMSGVarsWithConstant vs' = substFromList (map (\v -> (v, constant v)) vs)
