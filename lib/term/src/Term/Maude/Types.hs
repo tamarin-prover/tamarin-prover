00a282da74b507ff0386d820c760ef1d6582bfad
Kevin Milner <kamilner@kamilner.ca>
2026-06-17 11:36:00 +0100

    Canonicalise maude's returned substitution entries

--- lib/term/src/Term/Maude/Types.hs
+++ lib/term/src/Term/Maude/Types.hs
@@ -32,7 +32,9 @@ import           Control.Monad.Bind
 -- import           Control.Applicative
 
 -- import           Data.Traversable hiding (mapM)
+import           Data.List (sortBy)
 import           Data.Maybe
+import           Data.Ord (comparing)
 import qualified Data.Map as M
 import           Data.Map (Map)
 
@@ -129,7 +131,7 @@ msubstToLSubstVFresh bindings substMaude
         error $ "msubstToLSubstVFresh: nonfresh variables in `"++show substMaude++"'"
     | otherwise = removeRenamings $ substFromListVFresh slist
  where
-   slist = runBackConversion (traverse translate substMaude) bindings
+   slist = runBackConversion (traverse translate (sortBy (comparing (snd . fst)) substMaude)) bindings
    -- try to keep variable name for xi -> xj mappings
    -- commented out, seems wrong
    --  translate ((s,i), mt@(Lit (FreshVar _ _))) = do
